674188a2c38e6db159fd53d303d6e550428f8309
Tomas Tamantini <tomasbft@gmail.com>
2024-01-03 16:54:31 -0300

    Remove duplicate test

--- tests/tests_2015/test_a2015_d9.py
+++ tests/tests_2015/test_a2015_d9.py
@@ -15,14 +15,6 @@ def test_graph_finds_shortest_possible_itinerary():
     assert graph.shortest_complete_itinerary_distance() == 200
 
 
-def test_graph_finds_shortest_possible_itinerary():
-    graph = AdirectedGraph()
-    graph.add_edge("a", "b", 100)
-    graph.add_edge("a", "c", 100)
-    graph.add_edge("b", "c", 150)
-    assert graph.shortest_complete_itinerary_distance() == 200
-
-
 def test_graph_finds_longest_possible_itinerary():
     graph = AdirectedGraph()
     graph.add_edge("a", "b", 100)
