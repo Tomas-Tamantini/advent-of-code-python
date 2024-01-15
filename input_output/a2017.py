c337d3b37affbdf9eee83ed48e64652575246fa9
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-01-15 10:16:59 -0300

    Setup AOC 2017 solutions

---
+++ input_output/a2017.py
@@ -0,0 +1,152 @@
+from input_output.file_parser import FileParser
+
+parser = FileParser.default()
+
+
+# AOC 2017 Day 1: Inverse Captcha
+def aoc_2017_d1(file_name: str):
+    print("AOC 2017 Day 1/Part 1: Not implemented")
+    print("AOC 2017 Day 1/Part 2: Not implemented")
+
+
+# AOC 2017 Day 2: Corruption Checksum
+def aoc_2017_d2(file_name: str):
+    print("AOC 2017 Day 2/Part 1: Not implemented")
+    print("AOC 2017 Day 2/Part 2: Not implemented")
+
+
+# AOC 2017 Day 3: Spiral Memory
+def aoc_2017_d3(file_name: str):
+    print("AOC 2017 Day 3/Part 1: Not implemented")
+    print("AOC 2017 Day 3/Part 2: Not implemented")
+
+
+# AOC 2017 Day 4: High-Entropy Passphrases
+def aoc_2017_d4(file_name: str):
+    print("AOC 2017 Day 4/Part 1: Not implemented")
+    print("AOC 2017 Day 4/Part 2: Not implemented")
+
+
+# AOC 2017 Day 5: A Maze of Twisty Trampolines, All Alike
+def aoc_2017_d5(file_name: str):
+    print("AOC 2017 Day 5/Part 1: Not implemented")
+    print("AOC 2017 Day 5/Part 2: Not implemented")
+
+
+# AOC 2017 Day 6: Memory Reallocation
+def aoc_2017_d6(file_name: str):
+    print("AOC 2017 Day 6/Part 1: Not implemented")
+    print("AOC 2017 Day 6/Part 2: Not implemented")
+
+
+# AOC 2017 Day 7: Recursive Circus
+def aoc_2017_d7(file_name: str):
+    print("AOC 2017 Day 7/Part 1: Not implemented")
+    print("AOC 2017 Day 7/Part 2: Not implemented")
+
+
+# AOC 2017 Day 8: I Heard You Like Registers
+def aoc_2017_d8(file_name: str):
+    print("AOC 2017 Day 8/Part 1: Not implemented")
+    print("AOC 2017 Day 8/Part 2: Not implemented")
+
+
+# AOC 2017 Day 9: Stream Processing
+def aoc_2017_d9(file_name: str):
+    print("AOC 2017 Day 9/Part 1: Not implemented")
+    print("AOC 2017 Day 9/Part 2: Not implemented")
+
+
+# AOC 2017 Day 10: Knot Hash
+def aoc_2017_d10(file_name: str):
+    print("AOC 2017 Day 10/Part 1: Not implemented")
+    print("AOC 2017 Day 10/Part 2: Not implemented")
+
+
+# AOC 2017 Day 11: Hex Ed
+def aoc_2017_d11(file_name: str):
+    print("AOC 2017 Day 11/Part 1: Not implemented")
+    print("AOC 2017 Day 11/Part 2: Not implemented")
+
+
+# AOC 2017 Day 12: Digital Plumber
+def aoc_2017_d12(file_name: str):
+    print("AOC 2017 Day 12/Part 1: Not implemented")
+    print("AOC 2017 Day 12/Part 2: Not implemented")
+
+
+# AOC 2017 Day 13: Packet Scanners
+def aoc_2017_d13(file_name: str):
+    print("AOC 2017 Day 13/Part 1: Not implemented")
+    print("AOC 2017 Day 13/Part 2: Not implemented")
+
+
+# AOC 2017 Day 14: Disk Defragmentation
+def aoc_2017_d14(file_name: str):
+    print("AOC 2017 Day 14/Part 1: Not implemented")
+    print("AOC 2017 Day 14/Part 2: Not implemented")
+
+
+# AOC 2017 Day 15: Dueling Generators
+def aoc_2017_d15(file_name: str):
+    print("AOC 2017 Day 15/Part 1: Not implemented")
+    print("AOC 2017 Day 15/Part 2: Not implemented")
+
+
+# AOC 2017 Day 16: Permutation Promenade
+def aoc_2017_d16(file_name: str):
+    print("AOC 2017 Day 16/Part 1: Not implemented")
+    print("AOC 2017 Day 16/Part 2: Not implemented")
+
+
+# AOC 2017 Day 17: Spinlock
+def aoc_2017_d17(file_name: str):
+    print("AOC 2017 Day 17/Part 1: Not implemented")
+    print("AOC 2017 Day 17/Part 2: Not implemented")
+
+
+# AOC 2017 Day 18: Duet
+def aoc_2017_d18(file_name: str):
+    print("AOC 2017 Day 18/Part 1: Not implemented")
+    print("AOC 2017 Day 18/Part 2: Not implemented")
+
+
+# AOC 2017 Day 19: A Series of Tubes
+def aoc_2017_d19(file_name: str):
+    print("AOC 2017 Day 19/Part 1: Not implemented")
+    print("AOC 2017 Day 19/Part 2: Not implemented")
+
+
+# AOC 2017 Day 20: Particle Swarm
+def aoc_2017_d20(file_name: str):
+    print("AOC 2017 Day 20/Part 1: Not implemented")
+    print("AOC 2017 Day 20/Part 2: Not implemented")
+
+
+# AOC 2017 Day 21: Fractal Art
+def aoc_2017_d21(file_name: str):
+    print("AOC 2017 Day 21/Part 1: Not implemented")
+    print("AOC 2017 Day 21/Part 2: Not implemented")
+
+
+# AOC 2017 Day 22: Sporifica Virus
+def aoc_2017_d22(file_name: str):
+    print("AOC 2017 Day 22/Part 1: Not implemented")
+    print("AOC 2017 Day 22/Part 2: Not implemented")
+
+
+# AOC 2017 Day 23: Coprocessor Conflagration
+def aoc_2017_d23(file_name: str):
+    print("AOC 2017 Day 23/Part 1: Not implemented")
+    print("AOC 2017 Day 23/Part 2: Not implemented")
+
+
+# AOC 2017 Day 24: Electromagnetic Moat
+def aoc_2017_d24(file_name: str):
+    print("AOC 2017 Day 24/Part 1: Not implemented")
+    print("AOC 2017 Day 24/Part 2: Not implemented")
+
+
+# AOC 2017 Day 25: The Halting Problem
+def aoc_2017_d25(file_name: str):
+    print("AOC 2017 Day 25: Not implemented")
--- input_output/run_solutions.py
+++ input_output/run_solutions.py
@@ -1,5 +1,6 @@
 from .a2015 import *
 from .a2016 import *
+from .a2017 import *
 
 
 def run_solutions(problems: dict[int, tuple[int, ...]]) -> None:
@@ -57,6 +58,33 @@ def run_solutions(problems: dict[int, tuple[int, ...]]) -> None:
             aoc_2016_d23,
             aoc_2016_d24,
             aoc_2016_d25,
+        ),
+        2017: (
+            aoc_2017_d1,
+            aoc_2017_d2,
+            aoc_2017_d3,
+            aoc_2017_d4,
+            aoc_2017_d5,
+            aoc_2017_d6,
+            aoc_2017_d7,
+            aoc_2017_d8,
+            aoc_2017_d9,
+            aoc_2017_d10,
+            aoc_2017_d11,
+            aoc_2017_d12,
+            aoc_2017_d13,
+            aoc_2017_d14,
+            aoc_2017_d15,
+            aoc_2017_d16,
+            aoc_2017_d17,
+            aoc_2017_d18,
+            aoc_2017_d19,
+            aoc_2017_d20,
+            aoc_2017_d21,
+            aoc_2017_d22,
+            aoc_2017_d23,
+            aoc_2017_d24,
+            aoc_2017_d25,
         ),
     }
     for year, days in problems.items():
