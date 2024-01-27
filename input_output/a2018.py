d42fce77539f74223546c60ffb929c8148863ae2
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-01-27 17:05:48 -0300

    AOC 2018 - Day 12/Part 2

--- input_output/a2018.py
+++ input_output/a2018.py
@@ -182,6 +182,22 @@ def aoc_2018_d12(file_name: str):
     print(
         f"AOC 2018 Day 12/Part 1: Sum of indices of plants alive: {sum(plants_alive)}"
     )
+    # Part 2 assumes linear growth after transitional period
+    last_alive = 0
+    diff_seq = []
+    generation = 0
+    while True:
+        alive = sum(plant_automaton.plants_alive(generation=generation))
+        new_diff = alive - last_alive
+        last_alive = alive
+        diff_seq.append(new_diff)
+        if len(diff_seq) > 3 and all(d == new_diff for d in diff_seq[-3:]):
+            break
+        generation += 1
+    a_50_billion = alive + (50_000_000_000 - generation) * diff_seq[-1]
+    print(
+        f"AOC 2018 Day 12/Part 2: Sum of indices of plants alive after 50 billion generations: {a_50_billion}"
+    )
 
 
 # AOC 2018 Day 13: Mine Cart Madness
