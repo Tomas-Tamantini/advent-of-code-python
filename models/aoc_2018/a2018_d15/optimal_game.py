e032a7699e50f1d101c66e2b1692a08caa08d604
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-01-30 10:42:18 -0300

    Remove boundary limitation for optimal cave game

--- models/aoc_2018/a2018_d15/optimal_game.py
+++ models/aoc_2018/a2018_d15/optimal_game.py
@@ -11,7 +11,7 @@ class OptimalGameResults:
 
 
 def optimal_game_for_elves(
-    game: CaveGame, bot: CaveGameBot, binary_search_upper_bound: int = 100
+    game: CaveGame, bot: CaveGameBot, binary_search_upper_bound: int = 30
 ) -> OptimalGameResults:
     cave = game.cave_map
     initial_state = game.state
@@ -20,21 +20,21 @@ def optimal_game_for_elves(
     result = None
     while attack_power_lb < attack_power_ub:
         attack_power = (attack_power_lb + attack_power_ub) // 2
-        game = CaveGame(cave, initial_state.set_elf_attack_power(attack_power))
-        game.play_until_over(bot)
-        if game.elf_casualties == 0:
+        new_game = CaveGame(cave, initial_state.set_elf_attack_power(attack_power))
+        new_game.play_until_over(bot)
+        if new_game.elf_casualties == 0:
             attack_power_ub = attack_power
             if result is None or result.elf_attack_power > attack_power:
                 result = OptimalGameResults(
-                    rounds=game.round,
-                    hp_remaining=game.state.total_hp,
+                    rounds=new_game.round,
+                    hp_remaining=new_game.state.total_hp,
                     elf_attack_power=attack_power,
                 )
         else:
             attack_power_lb = attack_power + 1
 
     if result is None:
-        raise ValueError(
-            f"No optimal game found - Elf attack power must be greater than {binary_search_upper_bound}"
-        )
-    return result
+        new_boundary = binary_search_upper_bound * 10
+        return optimal_game_for_elves(game, bot, new_boundary)
+    else:
+        return result
