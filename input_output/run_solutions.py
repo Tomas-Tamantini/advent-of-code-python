141cf4d5d80c38ec9c391b0070235107c6953031
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-02-20 12:58:20 -0300

    Allow for --animate flag

--- input_output/run_solutions.py
+++ input_output/run_solutions.py
@@ -5,7 +5,7 @@ from .a2018 import ALL_2018_SOLUTIONS
 from .a2019 import ALL_2019_SOLUTIONS
 
 
-def run_solutions(problems: dict[int, tuple[int, ...]]) -> None:
+def run_solutions(problems: dict[int, tuple[int, ...]], animate: bool) -> None:
     solutions = {
         2015: ALL_2015_SOLUTIONS,
         2016: ALL_2016_SOLUTIONS,
@@ -18,4 +18,7 @@ def run_solutions(problems: dict[int, tuple[int, ...]]) -> None:
             days = [i + 1 for i in range(len(solutions[year]))]
         for day in days:
             file_name = f"input_files/aoc_{year}/a{year}_d{day}.txt"
-            solutions[year][day - 1](file_name)
+            try:
+                solutions[year][day - 1](file_name, animate=animate)
+            except TypeError:
+                solutions[year][day - 1](file_name)
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import sys
 from input_output import run_solutions
 
-solutions_to_run = {
-    2019: (),
-}
 
-run_solutions(solutions_to_run)
+def main(animate: bool) -> None:
+    solutions_to_run = {
+        2019: (),
+    }
+
+    run_solutions(solutions_to_run, animate)
+
+
+if __name__ == "__main__":
+    main(animate="--animate" in sys.argv)
