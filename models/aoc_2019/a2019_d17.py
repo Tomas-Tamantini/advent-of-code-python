734006d2fe85e6f3bfe4bb07bafb29ae67ccd9b9
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-02-22 14:12:52 -0300

    AOC 2019 - Day 17 - Implement path finding algorithm

--- models/aoc_2019/a2019_d17.py
+++ models/aoc_2019/a2019_d17.py
@@ -1,49 +1,141 @@
-from typing import Iterator
-from models.vectors import Vector2D
+from math import inf
+from typing import Iterator, Optional
+from dataclasses import dataclass
+from models.vectors import Vector2D, TurnDirection, CardinalDirection
 from .intcode import IntcodeProgram, run_intcode_program
+
+
+@dataclass(frozen=True)
+class VacuumRobotInstruction:
+    turn: TurnDirection
+    steps: int
+
+    def __str__(self) -> str:
+        return f"{self.turn.value},{self.steps}"
+
+
+@dataclass(frozen=True)
+class _VacuumRobot:
+    position: Vector2D
+    direction: CardinalDirection
+
+    def new_position(self, instruction: VacuumRobotInstruction) -> Vector2D:
+        new_direction = self.direction.turn(instruction.turn)
+        new_position = self.position.move(
+            new_direction, num_steps=instruction.steps, y_grows_down=True
+        )
+        return new_position
+
+    def move(self, instruction: VacuumRobotInstruction) -> "_VacuumRobot":
+        new_direction = self.direction.turn(instruction.turn)
+        new_position = self.position.move(
+            new_direction, num_steps=instruction.steps, y_grows_down=True
+        )
+        return _VacuumRobot(new_position, new_direction)
 
 
 class ScaffoldMap:
     def __init__(self) -> None:
-        self._map = ""
+        self._map = [[]]
+        self._vacuum_robot = None
+
+    @property
+    def vacuum_robot(self) -> _VacuumRobot:
+        return self._vacuum_robot
 
     def add_pixel(self, pixel: str) -> None:
-        self._map += pixel
+        if pixel == "\n":
+            self._map.append([])
+        else:
+            self._map[-1].append(pixel)
+            if pixel in "^v<>":
+                bot_position = Vector2D(len(self._map[-1]) - 1, len(self._map) - 1)
+                bot_direction = {
+                    "^": CardinalDirection.NORTH,
+                    "v": CardinalDirection.SOUTH,
+                    "<": CardinalDirection.WEST,
+                    ">": CardinalDirection.EAST,
+                }[pixel]
+                self._vacuum_robot = _VacuumRobot(bot_position, bot_direction)
 
     def render(self) -> str:
-        return self._map
+        return "\n".join("".join(line) for line in self._map)
 
-    def _pixels(self) -> list[list[chr]]:
-        return [list(line) for line in self._map.split("\n")]
-
-    def _is_scaffold(self, pixels: list[list[chr]], pos: Vector2D) -> bool:
+    def _is_scaffold(self, pos: Vector2D) -> bool:
         if (
             pos.x < 0
             or pos.y < 0
-            or pos.y >= len(pixels)
-            or pos.x >= len(pixels[pos.y])
+            or pos.y >= len(self._map)
+            or pos.x >= len(self._map[pos.y])
         ):
             return False
-        return pixels[pos.y][pos.x] in "#^v<>"
+        return self._map[pos.y][pos.x] in "#^v<>"
 
-    def _is_intersection(self, pixels: list[list[chr]], position: Vector2D) -> bool:
-        if not self._is_scaffold(pixels, position):
+    def _is_intersection(self, position: Vector2D) -> bool:
+        if not self._is_scaffold(position):
             return False
         scaffold_neighbors = [
             neighbor
             for neighbor in position.adjacent_positions()
-            if self._is_scaffold(pixels, neighbor)
+            if self._is_scaffold(neighbor)
         ]
 
         return len(scaffold_neighbors) > 2
 
     def scaffolding_intersections(self) -> Iterator[Vector2D]:
-        pixels = self._pixels()
-        for y, line in enumerate(pixels):
+        for y, line in enumerate(self._map):
             for x, _ in enumerate(line):
                 position = Vector2D(x, y)
-                if self._is_intersection(pixels, position):
+                if self._is_intersection(position):
                     yield position
+
+    def _next_turn_direction(self) -> Optional[TurnDirection]:
+        candidates = (TurnDirection.LEFT, TurnDirection.RIGHT, TurnDirection.NO_TURN)
+        for turn_direction in candidates:
+            new_position = self._vacuum_robot.new_position(
+                VacuumRobotInstruction(turn_direction, steps=1)
+            )
+            if self._is_scaffold(new_position):
+                return turn_direction
+
+    def _next_bot_movement(self) -> Optional[VacuumRobotInstruction]:
+        turn_direction = self._next_turn_direction()
+        if turn_direction is None:
+            return None
+        steps = 1
+        while self._is_scaffold(
+            self._vacuum_robot.new_position(
+                VacuumRobotInstruction(turn_direction, steps + 1)
+            )
+        ):
+            steps += 1
+
+        return VacuumRobotInstruction(turn_direction, steps)
+
+    def path_through_scaffolding(self) -> Iterator[VacuumRobotInstruction]:
+        while (instruction := self._next_bot_movement()) is not None:
+            yield instruction
+            self._vacuum_robot = self._vacuum_robot.move(instruction)
+
+
+@dataclass
+class _CompressedPath:
+    main_routine: str
+    subroutines: dict[str, str]
+
+    @property
+    def max_length(self) -> int:
+        return max(
+            len(self.main_routine),
+            max(len(subroutine) for subroutine in self.subroutines.values()),
+        )
+
+
+def compress_vacuum_bot_path(
+    instructions: list[VacuumRobotInstruction],
+    num_subroutines: int,
+) -> _CompressedPath:
+    raise NotImplementedError()
 
 
 class CameraOutput:
--- tests/tests_2019/test_a2019_d17.py
+++ tests/tests_2019/test_a2019_d17.py
@@ -1,5 +1,12 @@
-from models.vectors import Vector2D
-from models.aoc_2019.a2019_d17 import ScaffoldMap, CameraOutput, run_scaffolding_program
+import pytest
+from models.vectors import Vector2D, TurnDirection, CardinalDirection
+from models.aoc_2019.a2019_d17 import (
+    ScaffoldMap,
+    CameraOutput,
+    run_scaffolding_program,
+    VacuumRobotInstruction,
+    compress_vacuum_bot_path,
+)
 
 
 def test_scaffold_map_starts_empty():
@@ -20,25 +27,75 @@ def test_can_add_pixels_to_scaffold_map():
     assert sm.render() == "#.v\n###"
 
 
-def test_can_iterate_through_scaffolding_intersections():
+def _build_scaffold(map_str: str) -> ScaffoldMap:
     sm = ScaffoldMap()
-    sm.add_pixel("#")
-    sm.add_pixel(".")
-    sm.add_pixel("#")
-    sm.add_pixel(".")
-    sm.add_pixel("\n")
-    sm.add_pixel("#")
-    sm.add_pixel("#")
-    sm.add_pixel("#")
-    sm.add_pixel("#")
-    sm.add_pixel("\n")
-    sm.add_pixel("#")
-    sm.add_pixel(".")
-    sm.add_pixel("#")
-    sm.add_pixel(".")
-    sm.add_pixel("\n")
+    for line in map_str.split("\n"):
+        for pixel in line.strip():
+            sm.add_pixel(pixel)
+        sm.add_pixel("\n")
+    return sm
 
+
+def test_can_iterate_through_scaffolding_intersections():
+    sm = _build_scaffold("#.#.\n####\n#.#.\n")
     assert list(sm.scaffolding_intersections()) == [Vector2D(0, 1), Vector2D(2, 1)]
+
+
+def test_can_find_position_and_direction_of_vacuum_bot():
+    sm = _build_scaffold("...\n..^")
+    assert sm.vacuum_robot.position == Vector2D(2, 1)
+    assert sm.vacuum_robot.direction == CardinalDirection.NORTH
+
+
+_example_path = [
+    VacuumRobotInstruction(TurnDirection.RIGHT, 8),
+    VacuumRobotInstruction(TurnDirection.RIGHT, 8),
+    VacuumRobotInstruction(TurnDirection.RIGHT, 4),
+    VacuumRobotInstruction(TurnDirection.RIGHT, 4),
+    VacuumRobotInstruction(TurnDirection.RIGHT, 8),
+    VacuumRobotInstruction(TurnDirection.LEFT, 6),
+    VacuumRobotInstruction(TurnDirection.LEFT, 2),
+    VacuumRobotInstruction(TurnDirection.RIGHT, 4),
+    VacuumRobotInstruction(TurnDirection.RIGHT, 4),
+    VacuumRobotInstruction(TurnDirection.RIGHT, 8),
+    VacuumRobotInstruction(TurnDirection.RIGHT, 8),
+    VacuumRobotInstruction(TurnDirection.RIGHT, 8),
+    VacuumRobotInstruction(TurnDirection.LEFT, 6),
+    VacuumRobotInstruction(TurnDirection.LEFT, 2),
+]
+
+
+def test_can_find_path_through_scaffolding():
+    map_str = """#######...#####
+                 #.....#...#...#
+                 #.....#...#...#
+                 ......#...#...#
+                 ......#...###.#
+                 ......#.....#.#
+                 ^########...#.#
+                 ......#.#...#.#
+                 ......#########
+                 ........#...#..
+                 ....#########..
+                 ....#...#......
+                 ....#...#......
+                 ....#...#......
+                 ....#####......"""
+    sm = _build_scaffold(map_str)
+    assert list(sm.path_through_scaffolding()) == _example_path
+
+
+@pytest.mark.skip("Not implemented")
+def test_path_through_scaffolding_can_be_compressed():
+    expected_main_routine = "A,B,A,C,A,B,C,A,B,C"
+    expected_subroutines = {
+        "A": "R,8,R,8",
+        "B": "R,4,R,4,R,8",
+        "C": "L,6,L,2",
+    }
+    compressed = compress_vacuum_bot_path(_example_path, num_subroutines=3)
+    assert compressed.main_routine == expected_main_routine
+    assert compressed.subroutines == expected_subroutines
 
 
 def test_camera_output_converts_value_to_ascii_and_builds_map():
