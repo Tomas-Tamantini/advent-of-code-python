d9844e3f3e702fb380736f975c1ae2b76bc6b466
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-04-08 17:17:01 -0300

    AOC 2019 - Day 19 - Start implementing part 2

--- models/aoc_2019/a2019_d19.py
+++ models/aoc_2019/a2019_d19.py
@@ -1,5 +1,32 @@
 from typing import Iterator
+from math import floor
+from dataclasses import dataclass
+from models.vectors import Vector2D, CardinalDirection
 from models.aoc_2019.intcode import IntcodeProgram, run_intcode_program
+
+
+@dataclass(frozen=True)
+class _Square:
+    bottom_left: Vector2D
+    side_length: int
+
+    @property
+    def top_left(self) -> Vector2D:
+        return self.bottom_left.move(CardinalDirection.NORTH, self.side_length - 1)
+
+    @property
+    def bottom_right(self) -> Vector2D:
+        return self.bottom_left.move(CardinalDirection.EAST, self.side_length - 1)
+
+    def move(self, direction: CardinalDirection) -> "_Square":
+        return _Square(self.bottom_left.move(direction), self.side_length)
+
+    def bring_closer_to_origin(self, multiplicative_factor: float) -> "_Square":
+        new_position = Vector2D(
+            floor(self.bottom_left.x * multiplicative_factor),
+            floor(self.bottom_left.y * multiplicative_factor),
+        )
+        return _Square(new_position, self.side_length)
 
 
 class BeamArea:
@@ -8,45 +35,129 @@ class BeamArea:
         self._height = height
         self._points_attracted_to_beam = set()
 
-    def coordinates(self) -> Iterator[tuple[int, int]]:
+    def coordinates(self) -> Iterator[Vector2D]:
         for y in range(self._height):
             for x in range(self._width):
-                yield (x, y)
+                yield Vector2D(x, y)
 
-    def set_point_attracted_to_beam(self, x: int, y: int) -> None:
-        self._points_attracted_to_beam.add((x, y))
+    def set_point_attracted_to_beam(self, coordinates: Vector2D) -> None:
+        self._points_attracted_to_beam.add(coordinates)
 
     @property
     def num_points_attracted_to_beam(self) -> int:
         return len(self._points_attracted_to_beam)
 
+    def _linear_approximation_coefficients(self) -> tuple[float, float]:
+        a_coeff, b_coeff = None, None
+        for y in reversed(range(self._height)):
+            x_attracted_to_beam = [
+                x
+                for x in range(self._width)
+                if Vector2D(x, y) in self._points_attracted_to_beam
+            ]
+            if not x_attracted_to_beam:
+                continue
+            min_x, max_x = min(x_attracted_to_beam), max(x_attracted_to_beam)
+            if b_coeff is None:
+                b_coeff = y / min_x
+            if max_x < self._width - 1:
+                a_coeff = y / max_x
+                return a_coeff, b_coeff
+
+    def initial_guess_for_square_position(self, side_length: int) -> _Square:
+        a, b = self._linear_approximation_coefficients()
+        x = floor(side_length * (1 + a) / (b - a))
+        y = floor(side_length * a * (1 + b) / (b - a))
+        return _Square(Vector2D(x, y), side_length)
+
 
 class _BeamScannerInput:
-    def __init__(self, *coordinates: int) -> None:
+    def __init__(self, coordinates: Vector2D) -> None:
         self._coordinates = coordinates
         self._current_idx = 0
 
     def read(self) -> int:
-        value = self._coordinates[self._current_idx]
+        value = self._coordinates.x if self._current_idx == 0 else self._coordinates.y
         self._current_idx += 1
         return value
 
 
 class _BeamScannerOutput:
-    def __init__(self, beam_area: BeamArea, *coordinates: int) -> None:
-        self._beam_area = beam_area
-        self._coordinates = coordinates
+    def __init__(self) -> None:
+        self._value = None
 
     def write(self, value: int) -> None:
-        if value == 1:
-            self._beam_area.set_point_attracted_to_beam(*self._coordinates)
+        self._value = value
+
+    @property
+    def value(self) -> int:
+        return self._value
+
+
+def _run_scammer_for_coordinates(instructions: list[int], coordinates: Vector2D) -> int:
+    program = IntcodeProgram(instructions[:])
+    scanner_input = _BeamScannerInput(coordinates)
+    scanner_output = _BeamScannerOutput()
+    run_intcode_program(
+        program, serial_input=scanner_input, serial_output=scanner_output
+    )
+    return scanner_output.value
 
 
 def run_beam_scanner(instructions: list[int], beam_area: BeamArea) -> None:
     for coordinates in beam_area.coordinates():
-        program = IntcodeProgram(instructions[:])
-        scanner_input = _BeamScannerInput(*coordinates)
-        scanner_output = _BeamScannerOutput(beam_area, *coordinates)
-        run_intcode_program(
-            program, serial_input=scanner_input, serial_output=scanner_output
+        output = _run_scammer_for_coordinates(instructions, coordinates)
+        if output == 1:
+            beam_area.set_point_attracted_to_beam(coordinates)
+
+
+def square_closest_to_beam_source(
+    side_length: int, instructions: list[int], scanned_area: BeamArea
+) -> Vector2D:
+    guess = scanned_area.initial_guess_for_square_position(side_length)
+    while True:
+        # Take square further from the origin:
+        top_left_is_inside_beam = _run_scammer_for_coordinates(
+            instructions, guess.top_left
         )
+        if not top_left_is_inside_beam:
+            guess = guess.move(CardinalDirection.EAST)
+            continue
+        bottom_right_is_inside_beam = _run_scammer_for_coordinates(
+            instructions, guess.bottom_right
+        )
+        if not bottom_right_is_inside_beam:
+            guess = guess.move(CardinalDirection.NORTH)
+            continue
+
+        # Bring square closer to the origin:
+        candidate_guess = guess.move(CardinalDirection.WEST)
+        top_left_is_inside_beam = _run_scammer_for_coordinates(
+            instructions, candidate_guess.top_left
+        )
+        if top_left_is_inside_beam:
+            guess = candidate_guess
+            continue
+
+        candidate_guess = guess.move(CardinalDirection.SOUTH)
+        bottom_right_is_inside_beam = _run_scammer_for_coordinates(
+            instructions, candidate_guess.bottom_right
+        )
+        if bottom_right_is_inside_beam:
+            guess = candidate_guess
+            continue
+
+        candidate_guess = guess.move(CardinalDirection.WEST).move(
+            CardinalDirection.SOUTH
+        )
+        top_left_is_inside_beam = _run_scammer_for_coordinates(
+            instructions, candidate_guess.top_left
+        )
+        bottom_right_is_inside_beam = _run_scammer_for_coordinates(
+            instructions, candidate_guess.bottom_right
+        )
+        if top_left_is_inside_beam and bottom_right_is_inside_beam:
+            guess = candidate_guess
+            continue
+
+        return guess.bottom_left
--- tests/tests_2019/test_a2019_d19.py
+++ tests/tests_2019/test_a2019_d19.py
@@ -1,11 +1,19 @@
-from models.aoc_2019 import BeamArea, run_beam_scanner
+from models.vectors import Vector2D
+from models.aoc_2019 import BeamArea, run_beam_scanner, square_closest_to_beam_source
 
 
 def test_beam_area_iterates_through_its_xy_coordinates():
     area = BeamArea(width=3, height=2)
     coordinates = list(area.coordinates())
     assert len(coordinates) == 6
-    assert set(coordinates) == {(0, 0), (1, 0), (2, 0), (0, 1), (1, 1), (2, 1)}
+    assert set(coordinates) == {
+        Vector2D(0, 0),
+        Vector2D(1, 0),
+        Vector2D(2, 0),
+        Vector2D(0, 1),
+        Vector2D(1, 1),
+        Vector2D(2, 1),
+    }
 
 
 def test_no_points_are_attracted_to_beam_by_default():
@@ -15,8 +23,8 @@ def test_no_points_are_attracted_to_beam_by_default():
 
 def test_can_indicate_which_points_are_attracted_to_beam():
     area = BeamArea(width=3, height=3)
-    area.set_point_attracted_to_beam(x=1, y=1)
-    area.set_point_attracted_to_beam(x=2, y=2)
+    area.set_point_attracted_to_beam(Vector2D(1, 1))
+    area.set_point_attracted_to_beam(Vector2D(2, 2))
     assert area.num_points_attracted_to_beam == 2
 
 
@@ -26,3 +34,44 @@ def test_beam_scanner_runs_program_for_each_coordinate_in_beam_area():
     instructions = [3, 11, 3, 13, 8, 11, 12, 11, 4, 11, 99, -1, 1]
     run_beam_scanner(instructions, beam_area=area)
     assert area.num_points_attracted_to_beam == 4
+
+
+def test_beam_scanner_finds_closest_square_which_fully_fits_inside_beam():
+    scanned_area = BeamArea(width=10, height=10)
+    # Attraction only in y<x<2y coordinates:
+    instructions = [
+        3,
+        1_001,  # Input x
+        3,
+        1_002,  # Input y,
+        102,
+        2,
+        1_002,
+        1_003,  # Store 2*y
+        7,
+        1_002,
+        1_001,
+        1_004,  # y < x
+        7,
+        1_001,
+        1_003,
+        1_005,  # x < 2y
+        1,
+        1_004,
+        1_005,
+        1_006,  # y<x<2y
+        108,
+        2,
+        1_006,
+        1_007,  # 1 if y<x<2y else 0
+        4,
+        1_007,  # Output
+        99,
+    ]
+    run_beam_scanner(instructions, beam_area=scanned_area)
+    side_length = 4
+    square_position = square_closest_to_beam_source(
+        side_length, instructions, scanned_area
+    )
+    assert square_position.x == 12
+    assert square_position.y == 8
