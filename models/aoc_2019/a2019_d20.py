461d0fb62f5946c8fbb18fbfe80a4d9bb0d2cc6a
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-04-09 21:49:28 -0300

    AOC 2019 - Day 20 - Implement RecursiveDonutMaze class

--- models/aoc_2019/a2019_d20.py
+++ models/aoc_2019/a2019_d20.py
@@ -1,5 +1,8 @@
+from typing import Iterator
+from dataclasses import dataclass
+from math import inf
 from models.vectors import Vector2D
-from models.graphs import GridMaze
+from models.graphs import GridMaze, dijkstra
 
 
 class PortalMaze(GridMaze):
@@ -20,3 +23,76 @@ class PortalMaze(GridMaze):
     def num_steps_to_solve(self) -> int:
         self.reduce(irreducible_nodes={self._entrance, self._exit})
         return self.shortest_distance(self._entrance, self._exit)
+
+
+@dataclass(frozen=True)
+class DonutMazeNode:
+    position: Vector2D
+    level: int
+
+
+class RecursiveDonutMaze:
+    def __init__(self) -> None:
+        self._main_maze = GridMaze()
+        self._entrance = None
+        self._exit = None
+        self._step_up_to_step_down_portals = {}
+        self._step_down_to_step_up_portals = {}
+
+    def add_node(self, node: Vector2D) -> None:
+        self._main_maze.add_node(node)
+
+    def set_entrance(self, entrance: Vector2D) -> None:
+        self._entrance = entrance
+
+    def set_exit(self, exit: Vector2D) -> None:
+        self._exit = exit
+
+    def add_portal(self, step_up: Vector2D, step_down: Vector2D) -> None:
+        self._step_up_to_step_down_portals[step_up] = step_down
+        self._step_down_to_step_up_portals[step_down] = step_up
+
+    def irreducible_nodes(self) -> Iterator[Vector2D]:
+        yield self._entrance
+        yield self._exit
+        yield from self._step_up_to_step_down_portals.keys()
+        yield from self._step_down_to_step_up_portals.keys()
+
+    def neighbors(self, node: DonutMazeNode) -> Iterator[DonutMazeNode]:
+        for neighboring_position in self._main_maze.neighbors(node.position):
+            yield DonutMazeNode(neighboring_position, node.level)
+
+        if node.position in self._step_up_to_step_down_portals:
+            step_down = self._step_up_to_step_down_portals[node.position]
+            yield DonutMazeNode(step_down, node.level + 1)
+
+        if node.position in self._step_down_to_step_up_portals and node.level > 0:
+            step_up = self._step_down_to_step_up_portals[node.position]
+            yield DonutMazeNode(step_up, node.level - 1)
+
+    def _are_neighbors_from_different_levels(
+        self, node_a: DonutMazeNode, node_b: DonutMazeNode
+    ) -> bool:
+        return (
+            node_a.level == node_b.level - 1
+            and node_a.position
+            == self._step_down_to_step_up_portals.get(node_b.position, None)
+        ) or (
+            node_a.level == node_b.level + 1
+            and node_a.position
+            == self._step_up_to_step_down_portals.get(node_b.position, None)
+        )
+
+    def weight(self, node_a: DonutMazeNode, node_b: DonutMazeNode) -> float:
+        if node_a.level == node_b.level:
+            return self._main_maze.weight(node_a.position, node_b.position)
+        return 1 if self._are_neighbors_from_different_levels(node_a, node_b) else inf
+
+    def num_steps_to_solve(self) -> int:
+        self._main_maze.reduce(irreducible_nodes=set(self.irreducible_nodes()))
+        origin = DonutMazeNode(self._entrance, level=0)
+        destination = DonutMazeNode(self._exit, level=0)
+        try:
+            return dijkstra(origin, destination, self)[1]
+        except ValueError:
+            return inf
--- tests/tests_2019/test_a2019_d20.py
+++ tests/tests_2019/test_a2019_d20.py
@@ -1,5 +1,5 @@
 from models.vectors import Vector2D
-from models.aoc_2019.a2019_d20 import PortalMaze
+from models.aoc_2019.a2019_d20 import PortalMaze, RecursiveDonutMaze
 
 
 def test_portal_maze_finds_shortest_path_between_entrance_and_exit():
@@ -20,3 +20,27 @@ def test_portal_maze_can_have_portals_between_non_neighboring_nodes():
     maze.set_exit(Vector2D(0, 9))
     maze.add_portal(Vector2D(0, 2), Vector2D(0, 7))
     assert maze.num_steps_to_solve() == 5
+
+
+def test_recursive_donut_maze_finds_shortest_path_between_entrance_and_exit():
+    maze = RecursiveDonutMaze()
+    maze_cells = [(0, 0), (0, 1), (1, 1), (2, 1), (1, 2)]
+    for cell in maze_cells:
+        maze.add_node(Vector2D(*cell))
+    maze.set_entrance(Vector2D(0, 0))
+    maze.set_exit(Vector2D(1, 2))
+    assert maze.num_steps_to_solve() == 3
+
+
+def test_recursive_donut_maze_has_portals_to_allow_passage_between_levels():
+    maze = RecursiveDonutMaze()
+    for i in range(5):
+        for j in range(3):
+            maze.add_node(Vector2D(i, 100 * j))
+
+    maze.set_entrance(Vector2D(0, 0))
+    maze.set_exit(Vector2D(3, 200))
+    maze.add_portal(step_up=Vector2D(2, 0), step_down=Vector2D(1, 100))
+    maze.add_portal(step_up=Vector2D(0, 100), step_down=Vector2D(4, 200))
+    maze.add_portal(step_up=Vector2D(0, 200), step_down=Vector2D(4, 100))
+    assert maze.num_steps_to_solve() == 10
