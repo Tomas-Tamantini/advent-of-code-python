3951b0fc54a95093c0c8613d4ecdf8cddb1558b8
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-04-12 11:49:02 -0300

    AOC 2019 - Day 22 - Treat shuffle as linear transformations

--- models/aoc_2019/a2019_d22.py
+++ models/aoc_2019/a2019_d22.py
@@ -1,15 +1,35 @@
 from typing import Protocol
+from dataclasses import dataclass
 
 
-class _Shuffle(Protocol):
+@dataclass(frozen=True)
+class _LinearCoefficients:
+    first_degree: int
+    zeroth_degree: int
+
+    def compose(self, other: "_LinearCoefficients") -> "_LinearCoefficients":
+        return _LinearCoefficients(
+            first_degree=self.first_degree * other.first_degree,
+            zeroth_degree=other.first_degree * self.zeroth_degree + other.zeroth_degree,
+        )
+
+
+class _LinearShuffle(Protocol):
     def new_card_position(
         self, position_before_shuffle: int, deck_size: int
     ) -> int: ...
+
+    @property
+    def linear_coefficients(self) -> _LinearCoefficients: ...
 
 
 class DealIntoNewStackShuffle:
     def new_card_position(self, position_before_shuffle: int, deck_size: int) -> int:
         return deck_size - position_before_shuffle - 1
+
+    @property
+    def linear_coefficients(self) -> _LinearCoefficients:
+        return _LinearCoefficients(-1, -1)
 
 
 class CutCardsShuffle:
@@ -19,6 +39,10 @@ class CutCardsShuffle:
     def new_card_position(self, position_before_shuffle: int, deck_size: int) -> int:
         return (position_before_shuffle - self._num_cards_to_cut) % deck_size
 
+    @property
+    def linear_coefficients(self) -> _LinearCoefficients:
+        return _LinearCoefficients(1, -self._num_cards_to_cut)
+
 
 class DealWithIncrementShuffle:
     def __init__(self, increment: int) -> None:
@@ -27,14 +51,21 @@ class DealWithIncrementShuffle:
     def new_card_position(self, position_before_shuffle: int, deck_size: int) -> int:
         return (position_before_shuffle * self._increment) % deck_size
 
+    @property
+    def linear_coefficients(self) -> _LinearCoefficients:
+        return _LinearCoefficients(self._increment, 0)
+
 
 class MultiTechniqueShuffle:
-    def __init__(self, techniques: list[_Shuffle]) -> None:
-        self._techniques = techniques
+    def __init__(self, techniques: list[_LinearShuffle]) -> None:
+        self._linear_coefficients = _LinearCoefficients(1, 0)
+        for technique in techniques:
+            self._linear_coefficients = self._linear_coefficients.compose(
+                technique.linear_coefficients
+            )
 
     def new_card_position(self, position_before_shuffle: int, deck_size: int) -> int:
-        for technique in self._techniques:
-            position_before_shuffle = technique.new_card_position(
-                position_before_shuffle, deck_size
-            )
-        return position_before_shuffle
+        return (
+            self._linear_coefficients.first_degree * position_before_shuffle
+            + self._linear_coefficients.zeroth_degree
+        ) % deck_size
