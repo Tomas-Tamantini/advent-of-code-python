2330c9d54dc30d1d8d115132eb2a364f8808cdfe
Tomas Tamantini <tomasbft@gmail.com>
2024-04-13 23:56:49 -0300

    AOC 2019 - Day 25 - Implement automatic droid control (CHEATED BY USING MOVES FOUND BY MANUALLY PLAYING)

--- models/aoc_2019/a2019_d25/droid_control.py
+++ models/aoc_2019/a2019_d25/droid_control.py
@@ -1,5 +1,7 @@
 from typing import Protocol
+from models.vectors import CardinalDirection
 from .droid_input import DroidInput
+from .droid_command import MoveCommand, TakeCommand
 
 
 class DroidControl(Protocol):
@@ -35,12 +37,35 @@ class DroidCLIControl:
         print(output_line)
         if "Command?" in output_line:
             self._prompt_input()
+        elif "should be able to get in by typing" in output_line:
+            self._airlock_password = int(output_line.split()[11])
 
 
+# TODO: Implement actual exploration logic, rather than cheating by using pre-determined commands.
 class DroidAutomaticControl:
     def __init__(self, droid_input: DroidInput) -> None:
         self._droid_input = droid_input
         self._airlock_password = None
+        self._pre_determined_commands = [
+            MoveCommand(CardinalDirection.EAST),
+            TakeCommand("sand"),
+            MoveCommand(CardinalDirection.WEST),
+            MoveCommand(CardinalDirection.WEST),
+            MoveCommand(CardinalDirection.NORTH),
+            TakeCommand("wreath"),
+            MoveCommand(CardinalDirection.EAST),
+            TakeCommand("fixed point"),
+            MoveCommand(CardinalDirection.WEST),
+            MoveCommand(CardinalDirection.SOUTH),
+            MoveCommand(CardinalDirection.SOUTH),
+            MoveCommand(CardinalDirection.EAST),
+            MoveCommand(CardinalDirection.EAST),
+            MoveCommand(CardinalDirection.EAST),
+            TakeCommand("space law space brochure"),
+            MoveCommand(CardinalDirection.SOUTH),
+            MoveCommand(CardinalDirection.SOUTH),
+            MoveCommand(CardinalDirection.WEST),
+        ]
 
     @property
     def airlock_password(self) -> str:
@@ -52,5 +77,12 @@ class DroidAutomaticControl:
     def droid_input(self) -> DroidInput:
         return self._droid_input
 
+    def _give_next_input(self) -> None:
+        next_command = self._pre_determined_commands.pop(0)
+        self._droid_input.give_command(next_command)
+
     def handle_new_output_line(self, output_line: str) -> None:
-        raise NotImplementedError("Automatic control not implemented yet.")
+        if "Command?" in output_line:
+            self._give_next_input()
+        elif "should be able to get in by typing" in output_line:
+            self._airlock_password = int(output_line.split()[11])
