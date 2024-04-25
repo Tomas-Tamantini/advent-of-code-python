40673f7fff65d52b839eb5324e7e77bf5f62db81
Tomas Tamantini <tomasbft@gmail.com>
2024-04-25 19:25:29 -0300

    Add progress bar to modular logarithm

--- models/number_theory/modular_logarithm.py
+++ models/number_theory/modular_logarithm.py
@@ -1,5 +1,16 @@
-def modular_logarithm(number: int, base: int, mod: int) -> int:
+from typing import Optional
+from models.progress_bar_protocol import ProgressBar
+
+
+def modular_logarithm(
+    number: int,
+    base: int,
+    mod: int,
+    progress_bar: Optional[ProgressBar] = None,
+) -> int:
     for i in range(mod + 1):
+        if progress_bar:
+            progress_bar.update(i, mod + 1)
         if pow(base, i, mod) == number:
             return i
     raise ValueError(f"Cannot raise {base} to any power to make {number} mod {mod}")
