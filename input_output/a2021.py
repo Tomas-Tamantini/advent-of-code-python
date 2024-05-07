5f7c28920e0a63b4bf50311f90e010eab81a6f85
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-05-07 14:46:23 -0300

    AOC 2021 - Day 18 / Part 2

--- input_output/a2021.py
+++ input_output/a2021.py
@@ -385,6 +385,19 @@ def aoc_2021_d18(file_name: str, **_):
         f"AOC 2021 Day 18/Part 1: The magnitude of the snailfish is {acc.magnitude()}"
     )
 
+    largest_magnitude = 0
+    for i in range(len(lists)):
+        for j in range(len(lists)):
+            if i != j:
+                tree_i = SnailFishTree.from_list(lists[i])
+                tree_j = SnailFishTree.from_list(lists[j])
+                magnitude = (tree_i + tree_j).magnitude()
+                if magnitude > largest_magnitude:
+                    largest_magnitude = magnitude
+    print(
+        f"AOC 2021 Day 18/Part 2: The largest magnitude of the sum of two snailfish is {largest_magnitude}"
+    )
+
 
 # AOC 2021 - Day 19: Beacon Scanner
 def aoc_2021_d19(file_name: str, **_): ...
