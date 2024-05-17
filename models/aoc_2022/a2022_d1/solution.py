8876ea8854db5b4497b9fab70ddbf6c62823704d
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-05-17 13:54:34 -0300

    AOC 2022 - Day 1 - Add dashes to title

--- models/aoc_2022/a2022_d1/solution.py
+++ models/aoc_2022/a2022_d1/solution.py
@@ -2,7 +2,7 @@ from .parser import parse_calories
 
 
 def aoc_2022_d1(file_name: str, **_) -> None:
-    print("AOC 2022 - Day 1: Calorie Counting")
+    print("--- AOC 2022 - Day 1: Calorie Counting ---")
     calories_by_elf = [sum(calories) for calories in parse_calories(file_name)]
     sorted_calories = sorted(calories_by_elf)
     max_calories = sorted_calories[-1]
