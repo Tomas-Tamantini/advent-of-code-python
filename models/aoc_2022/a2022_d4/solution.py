84d52677e2bddc1dd439abacf258188505010d27
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-05-20 18:05:38 -0300

    AOC 2022 - Day 4 / Part 2

--- models/aoc_2022/a2022_d4/solution.py
+++ models/aoc_2022/a2022_d4/solution.py
@@ -4,12 +4,17 @@ from .parser import parse_interval_pairs
 
 def aoc_2022_d4(input_reader: InputReader, **_) -> None:
     print("--- AOC 2022 - Day 4: Camp Cleanup ---")
-    num_pairs_fully_contained = 0
-    for interval_a, interval_b in parse_interval_pairs(input_reader):
-        if interval_a.is_contained_by(interval_b) or interval_b.is_contained_by(
-            interval_a
-        ):
-            num_pairs_fully_contained += 1
+    pairs = list(parse_interval_pairs(input_reader))
+    num_pairs_fully_contained = sum(
+        interval_a.is_contained_by(interval_b) or interval_b.is_contained_by(interval_a)
+        for interval_a, interval_b in pairs
+    )
     print(
         f"Part 1: Number of pairs in which one interval contains the other is {num_pairs_fully_contained}"
     )
+
+    num_pairs_with_overlap = sum(
+        interval_a.intersection(interval_b) is not None
+        for interval_a, interval_b in pairs
+    )
+    print(f"Part 2: Number of pairs with some overlap {num_pairs_with_overlap}")
