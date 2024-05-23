b1d6e018b76c9940a2b485657207c5fbf276f79f
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-05-23 15:46:06 -0300

    AOC 2022 - Day 6

--- models/aoc_2022/a2022_d6/solution.py
+++ models/aoc_2022/a2022_d6/solution.py
@@ -1,5 +1,19 @@
 from models.common.io import InputReader
 
 
+def detect_distinct_chars(stream: str, num_distinct_chars: int) -> int:
+    for i in range(num_distinct_chars - 1, len(stream)):
+        current_window = stream[i - num_distinct_chars + 1 : i + 1]
+        if len(set(current_window)) == num_distinct_chars:
+            return i + 1
+
+    return -1
+
+
 def aoc_2022_d6(input_reader: InputReader, **_) -> None:
     print("--- AOC 2022 - Day 6: Tuning Trouble ---")
+    stream = input_reader.read()
+    start_of_packet = detect_distinct_chars(stream, num_distinct_chars=4)
+    print(f"Part 1: Packet starts at {start_of_packet}")
+    start_of_message = detect_distinct_chars(stream, num_distinct_chars=14)
+    print(f"Part 2: Message starts at {start_of_message}")
---
+++ models/aoc_2022/a2022_d6/test_detect_distinct_chars.py
@@ -0,0 +1,16 @@
+import pytest
+from .solution import detect_distinct_chars
+
+
+@pytest.mark.parametrize(
+    "stream, num_distinct_chars, expected",
+    [
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 4, 5),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4, 11),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14, 26),
+    ],
+)
+def test_can_detect_first_position_after_n_distinct_characters(
+    stream, num_distinct_chars, expected
+):
+    assert expected == detect_distinct_chars(stream, num_distinct_chars)
