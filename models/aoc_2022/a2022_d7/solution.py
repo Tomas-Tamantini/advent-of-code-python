49bf33d252432fd421df3921fc415e412533ac11
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-05-23 17:10:51 -0300

    AOC 2022 - Day 7 / Part 2

--- models/aoc_2022/a2022_d7/solution.py
+++ models/aoc_2022/a2022_d7/solution.py
@@ -5,9 +5,13 @@ from .parser import parse_file_tree
 def aoc_2022_d7(input_reader: InputReader, **_) -> None:
     print("--- AOC 2022 - Day 7: No Space Left On Device ---")
     tree = parse_file_tree(input_reader)
-    sum_small_directories = 0
-    for dir in tree.all_directories():
-        size = dir.size()
-        if size <= 100_000:
-            sum_small_directories += size
+    sizes = tuple(dir.size() for dir in tree.all_directories())
+    sum_small_directories = sum(size for size in sizes if size <= 100_000)
     print(f"Part 1: Sum of sizes of small directories is {sum_small_directories}")
+    tree.navigate_to_root()
+    used_space = tree.current_directory.size()
+    total_space = 70_000_000
+    required_space = 30_000_000
+    delete_threshold = required_space + used_space - total_space
+    size_to_delete = min((size for size in sizes if size >= delete_threshold))
+    print(f"Part 2: Size of smallest directory to delete is {size_to_delete}")
