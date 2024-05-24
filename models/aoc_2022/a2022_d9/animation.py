05d222bbd41fd7f3a462c68642904a8c8de49f1f
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-05-24 10:02:42 -0300

    AOC 2022 - Day 9 - Add animation

---
+++ models/aoc_2022/a2022_d9/animation.py
@@ -0,0 +1,54 @@
+from models.common.vectors import Vector2D
+from .logic import Rope
+
+
+class RopeAnimation:
+    def __init__(self, width: int, height: int, rope: Rope, total_num_iterations: int):
+        self._width = width
+        self._height = height
+        self._focus_length = min(width, height) // 4
+        self._head_to_tail = list(rope.positions_head_to_tail())
+        self._center = self._head_position
+        self._iteration = 0
+        self._visited_by_tail = {rope.tail_position}
+        self._total_num_iterations = total_num_iterations
+
+    @property
+    def _head_position(self) -> Vector2D:
+        return self._head_to_tail[0]
+
+    def update_rope(self, rope: Rope) -> None:
+        self._head_to_tail = list(rope.positions_head_to_tail())
+        self._visited_by_tail.add(rope.tail_position)
+        new_center_x = min(
+            max(self._center.x, self._head_position.x - self._focus_length),
+            self._head_position.x + self._focus_length,
+        )
+        new_center_y = min(
+            max(self._center.y, self._head_position.y - self._focus_length),
+            self._head_position.y + self._focus_length,
+        )
+        self._center = Vector2D(new_center_x, new_center_y)
+
+    def _is_landscape_marker(self, pos: Vector2D) -> bool:
+        return (pos.x + pos.y * pos.y) % 31 == (pos.x * pos.y) % 19
+
+    def frame(self) -> str:
+        x_start = self._center.x - self._width // 2
+        y_start = self._center.y - self._height // 2
+        frame = ""
+        for y in range(y_start, y_start + self._height):
+            for x in range(x_start, x_start + self._width):
+                pos = Vector2D(x, y)
+                if pos == self._head_position:
+                    frame += "X"
+                elif pos in self._head_to_tail:
+                    frame += "O"
+                elif self._is_landscape_marker(pos):
+                    frame += "*"
+                else:
+                    frame += "."
+            frame += "\n"
+        self._iteration += 1
+        frame += f"Iteration: {self._iteration}/{self._total_num_iterations} / Visited by tail: {len(self._visited_by_tail)}"
+        return frame
--- models/aoc_2022/a2022_d9/solution.py
+++ models/aoc_2022/a2022_d9/solution.py
@@ -1,30 +1,44 @@
-from typing import Iterator
-from models.common.io import InputReader
+from typing import Iterator, Optional
+from models.common.io import InputReader, render_frame
 from models.common.vectors import Vector2D, CardinalDirection
 from .logic import Rope
 from .parser import parse_move_instructions
+from .animation import RopeAnimation
 
 
 def _tail_positions(
-    rope: Rope, instructions: list[tuple[CardinalDirection, int]]
+    rope: Rope,
+    instructions: list[tuple[CardinalDirection, int]],
+    animation: Optional[RopeAnimation] = None,
 ) -> Iterator[Vector2D]:
     yield rope.tail_position
     for direction, distance in instructions:
         for _ in range(distance):
             rope.move_head(direction)
             yield rope.tail_position
+            if animation:
+                animation.update_rope(rope)
+                render_frame(animation.frame(), sleep_seconds=0.05)
 
 
-def aoc_2022_d9(input_reader: InputReader, **_) -> None:
+def aoc_2022_d9(input_reader: InputReader, animate: bool, **_) -> None:
     print("--- AOC 2022 - Day 9: Rope Bridge ---")
     instructions = list(parse_move_instructions(input_reader))
+    total_num_iterations = sum(distance for _, distance in instructions)
     short_rope = Rope(num_knots=2)
     tail_positions = set(_tail_positions(short_rope, instructions))
     print(
         f"Part 1: Unique positions visited by tail in two-knot rope is {len(tail_positions)}"
     )
     long_rope = Rope(num_knots=10)
-    tail_positions = set(_tail_positions(long_rope, instructions))
+    if animate:
+        animation_msg = ""
+        width = height = 15
+        animation = RopeAnimation(width, height, long_rope, total_num_iterations)
+    else:
+        animation_msg = " (SET FLAG --animate TO SEE COOL ANIMATION)"
+        animation = None
+    tail_positions = set(_tail_positions(long_rope, instructions, animation))
     print(
-        f"Part 2: Unique positions visited by tail in ten-knot rope is {len(tail_positions)}"
+        f"Part 2:{animation_msg} Unique positions visited by tail in ten-knot rope is {len(tail_positions)}"
     )
