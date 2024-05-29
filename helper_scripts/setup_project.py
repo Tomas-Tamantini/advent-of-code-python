077277023360ef53fcc1d47b98ebbf3148dba939
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-05-29 15:14:44 -0300

    Remove useless type hint

--- helper_scripts/setup_project.py
+++ helper_scripts/setup_project.py
@@ -63,6 +63,6 @@ def _create_parser_test_file(parser_method_name: str, test_path: str) -> None:
         f.write("from models.common.io import InputFromString\n")
         f.write(f"from ..parser import {parser_method_name}\n")
         f.write("\n\n")
-        f.write(f"def test_{parser_method_name}() -> None:\n")
+        f.write(f"def test_{parser_method_name}():\n")
         f.write('    input_reader = InputFromString("""file content""")\n')
         f.write("    assert False\n")
