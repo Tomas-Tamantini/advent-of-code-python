9ae67e789621a8eb66f2d9f969800badf69588b2
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-06-03 18:26:11 -0300

    AOC 2022 - Day 17 - Implement parser

---
+++ models/aoc_2022/a2022_d17/parser.py
@@ -0,0 +1,10 @@
+from typing import Iterator
+from models.common.io import InputReader
+from models.common.vectors import CardinalDirection
+
+
+def parse_wind_directions(input_reader: InputReader) -> Iterator[CardinalDirection]:
+    yield from (
+        CardinalDirection.EAST if c == ">" else CardinalDirection.WEST
+        for c in input_reader.read().strip()
+    )
---
+++ models/aoc_2022/a2022_d17/tests/test_parser.py
@@ -0,0 +1,13 @@
+from models.common.io import InputFromString
+from models.common.vectors import CardinalDirection
+from ..parser import parse_wind_directions
+
+
+def test_parse_wind_directions():
+    input_reader = InputFromString(">><")
+    result = list(parse_wind_directions(input_reader))
+    assert result == [
+        CardinalDirection.EAST,
+        CardinalDirection.EAST,
+        CardinalDirection.WEST,
+    ]
