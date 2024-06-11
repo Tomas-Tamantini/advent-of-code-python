5d1f37b559d1acf75dbc9e179cba3c11c1c4fe83
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-06-11 12:14:20 -0300

    Export VariableProtocol

--- models/common/optimization/linear_programming/__init__.py
+++ models/common/optimization/linear_programming/__init__.py
@@ -1,4 +1,10 @@
-from .variable import VariableId, ContinuousVariable, IntegerVariable, BinaryVariable
+from .variable import (
+    VariableId,
+    ContinuousVariable,
+    IntegerVariable,
+    BinaryVariable,
+    VariableProtocol,
+)
 from .constraint import Constraint, ConstraintType
 from .objective_function import ObjectiveFunction
 from .lp_solution import MilpSolution
