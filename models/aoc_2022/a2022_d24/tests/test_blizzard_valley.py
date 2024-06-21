b481ee0e97c22d985f1b02ac7fa08764abb34120
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-06-21 09:05:03 -0300

    AOC 2022 - Day 24 - Remove test duplication

--- models/aoc_2022/a2022_d24/tests/test_blizzard_valley.py
+++ models/aoc_2022/a2022_d24/tests/test_blizzard_valley.py
@@ -2,14 +2,18 @@ from models.common.vectors import Vector2D, CardinalDirection
 from ..logic import BlizzardValley, Blizzard
 
 
+def _build_blizzard_valley(
+    height: int = 3,
+    width: int = 4,
+    entrance: Vector2D = Vector2D(1, 0),
+    exit: Vector2D = Vector2D(2, 2),
+    blizzards: set[Blizzard] = None,
+):
+    return BlizzardValley(height, width, entrance, exit, blizzards=blizzards or set())
+
+
 def test_blizzard_valley_has_walls_around():
-    valley = BlizzardValley(
-        height=3,
-        width=4,
-        entrance=Vector2D(1, 0),
-        exit=Vector2D(2, 2),
-        blizzards=set(),
-    )
+    valley = _build_blizzard_valley(height=3, width=4)
     assert not valley.is_wall(Vector2D(1, 1))
     assert valley.is_wall(Vector2D(0, 1))
     assert valley.is_wall(Vector2D(3, 1))
@@ -18,24 +22,16 @@ def test_blizzard_valley_has_walls_around():
 
 
 def test_blizzard_valley_has_gap_in_wall_at_entrance_and_exit():
-    valley = BlizzardValley(
-        height=3,
-        width=4,
-        entrance=Vector2D(1, 0),
-        exit=Vector2D(2, 2),
-        blizzards=set(),
+    valley = _build_blizzard_valley(
+        height=3, width=4, entrance=Vector2D(1, 0), exit=Vector2D(2, 2)
     )
     assert not valley.is_wall(Vector2D(1, 0))
     assert not valley.is_wall(Vector2D(2, 2))
 
 
 def test_position_is_not_free_at_blizzard_valley_if_wall():
-    valley = BlizzardValley(
-        height=3,
-        width=4,
-        entrance=Vector2D(1, 0),
-        exit=Vector2D(2, 2),
-        blizzards=set(),
+    valley = _build_blizzard_valley(
+        height=3, width=4, entrance=Vector2D(1, 0), exit=Vector2D(2, 2)
     )
     assert not valley.position_is_free_at_time(position=Vector2D(0, 0), time=0)
     assert valley.position_is_free_at_time(position=Vector2D(1, 0), time=0)
@@ -46,13 +42,7 @@ def test_position_is_not_free_at_blizzard_valley_if_blizzard_is_there_at_that_ti
         initial_position=Vector2D(1, 1),
         direction=CardinalDirection.EAST,
     )
-    valley = BlizzardValley(
-        height=3,
-        width=4,
-        entrance=Vector2D(1, 0),
-        exit=Vector2D(2, 2),
-        blizzards={blizzard},
-    )
+    valley = _build_blizzard_valley(height=3, width=4, blizzards={blizzard})
     assert not valley.position_is_free_at_time(position=Vector2D(1, 1), time=0)
     assert valley.position_is_free_at_time(position=Vector2D(1, 1), time=1)
 
@@ -62,12 +52,6 @@ def test_blizzard_wraps_around_valley_like_pacman():
         initial_position=Vector2D(1, 1),
         direction=CardinalDirection.EAST,
     )
-    valley = BlizzardValley(
-        height=3,
-        width=4,
-        entrance=Vector2D(1, 0),
-        exit=Vector2D(2, 2),
-        blizzards={blizzard},
-    )
+    valley = _build_blizzard_valley(height=3, width=4, blizzards={blizzard})
     assert not valley.position_is_free_at_time(position=Vector2D(1, 1), time=1000)
     assert valley.position_is_free_at_time(position=Vector2D(1, 1), time=1001)
