77162f8ac6a2777863f28bc33d40adb93e4affa1
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-07-01 16:55:28 -0300

    AOC 2022 - Day 10 - Fix test to fit new output pixels

--- models/aoc_2022/a2022_d10/tests/test_sprite_screen.py
+++ models/aoc_2022/a2022_d10/tests/test_sprite_screen.py
@@ -3,7 +3,7 @@ from ..logic import SpriteScreen
 
 def test_sprite_screen_draws_one_pixel_per_cycle_checking_if_sprite_occupies_that_pixel():
     sprite_center_positions = [1, 1, 16, 16, 5, 5]
-    expected = "##..##"
+    expected = "##  ##"
     screen = SpriteScreen(width=6, height=1, sprite_length=3)
     message = screen.draw(sprite_center_positions)
     assert message == expected
