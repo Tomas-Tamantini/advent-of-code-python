42c0aa518a08cf7c2510c4f743014c476c2c094b
Tomas Tamantini <tomasbft@gmail.com>
2024-09-27 17:26:51 -0300

    AOC 2023 - Day 9

--- models/aoc_2023/a2023_d9/solution.py
+++ models/aoc_2023/a2023_d9/solution.py
@@ -1,8 +1,49 @@
 from typing import Iterator
-from models.common.io import IOHandler, Problem, ProblemSolution
+from models.common.io import IOHandler, Problem, ProblemSolution, InputReader
+
+
+def _difference(sequence: tuple[int, ...]) -> tuple[int, ...]:
+    return tuple(sequence[i + 1] - sequence[i] for i in range(len(sequence) - 1))
+
+
+def _differences(sequence: tuple[int, ...]) -> Iterator[tuple[int, ...]]:
+    yield sequence
+    while len(set(sequence)) > 1:
+        sequence = _difference(sequence)
+        yield sequence
+
+
+def next_in_sequence(sequence: tuple[int, ...]) -> int:
+    differences = list(_differences(sequence))
+    return sum(diff[-1] for diff in differences)
+
+
+def previous_in_sequence(sequence: tuple[int, ...]) -> int:
+    differences = list(_differences(sequence))
+    return sum(diff[0] * (1 - 2 * (i % 2)) for i, diff in enumerate(differences))
+
+
+def _parse_sequences(input_reader: InputReader) -> Iterator[tuple[int, ...]]:
+    for line in input_reader.read_stripped_lines():
+        yield tuple(map(int, line.split()))
 
 
 def aoc_2023_d9(io_handler: IOHandler) -> Iterator[ProblemSolution]:
     problem_id = Problem(2023, 9, "Mirage Maintenance")
     io_handler.output_writer.write_header(problem_id)
-    yield from []
+    sequences = tuple(_parse_sequences(io_handler.input_reader))
+    extrapolate_forward = sum(next_in_sequence(sequence) for sequence in sequences)
+    yield ProblemSolution(
+        problem_id,
+        f"The sum of values extrapolated forward is {extrapolate_forward}.",
+        result=extrapolate_forward,
+        part=1,
+    )
+
+    extrapolate_backward = sum(previous_in_sequence(sequence) for sequence in sequences)
+    yield ProblemSolution(
+        problem_id,
+        f"The sum of values extrapolated backward is {extrapolate_backward}.",
+        result=extrapolate_backward,
+        part=2,
+    )
---
+++ models/aoc_2023/a2023_d9/test_solution.py
@@ -0,0 +1,34 @@
+import pytest
+from .solution import next_in_sequence, previous_in_sequence
+
+
+@pytest.mark.parametrize(
+    "sequence, expected",
+    [
+        ((0, 0, 0), 0),
+        ((0, 3, 6, 9, 12, 15), 18),
+        ((1, 3, 6, 10, 15, 21), 28),
+        ((10, 13, 16, 21, 30, 45), 68),
+        ((1, -1, 2, -2), -25),
+    ],
+)
+def test_next_in_sequence_extrapolates_using_newton_forward_difference(
+    sequence, expected
+):
+    assert expected == next_in_sequence(sequence)
+
+
+@pytest.mark.parametrize(
+    "sequence, expected",
+    [
+        ((13, 13, 13), 13),
+        ((0, 3, 6, 9, 12, 15), -3),
+        ((1, 3, 6, 10, 15, 21), 0),
+        ((10, 13, 16, 21, 30, 45, 68), 5),
+        ((1, -1, 2, -2), 20),
+    ],
+)
+def test_previous_in_sequence_extrapolates_using_newton_backward_difference(
+    sequence, expected
+):
+    assert expected == previous_in_sequence(sequence)
