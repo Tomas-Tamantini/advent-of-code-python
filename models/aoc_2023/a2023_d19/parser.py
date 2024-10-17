a9721956ece933384ab5effcf476c0cbe724320c
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-10-17 09:27:27 -0300

    AOC 2023 - Day 19 - Implement parser

--- models/aoc_2023/a2023_d19/parser.py
+++ models/aoc_2023/a2023_d19/parser.py
@@ -1,5 +1,53 @@
+from typing import Iterator
 from models.common.io import InputReader
+from models.common.number_theory import Interval
+from .logic import (
+    Workflow,
+    LessThanRule,
+    GreaterThanRule,
+    WorkflowRule,
+    MachinePartRange,
+)
 
 
-def parse_workflows(input_reader: InputReader) -> "InsertReturnType":
-    pass
+def _parse_inequality_rule(rule_str: str) -> WorkflowRule:
+    rule_parts = rule_str.split(":")
+    next_workflow_id = rule_parts[-1].strip()
+    inequality_sign = ">" if ">" in rule_parts[0] else "<"
+    inequality_parts = rule_parts[0].split(inequality_sign)
+    attribute_name = inequality_parts[0]
+    threshold = int(inequality_parts[1])
+    if inequality_sign == ">":
+        return GreaterThanRule(attribute_name, threshold, next_workflow_id)
+    else:
+        return LessThanRule(attribute_name, threshold, next_workflow_id)
+
+
+def _parse_workflow(line: str) -> Workflow:
+    parts = line.split("{")
+    workflow_id = parts[0]
+    rules_repr = parts[1].replace("}", "").split(",")
+    default_next_workflow_id = rules_repr.pop()
+    rules = tuple(_parse_inequality_rule(rule) for rule in rules_repr)
+    return Workflow(workflow_id, rules, default_next_workflow_id)
+
+
+def parse_workflows(input_reader: InputReader) -> Iterator[Workflow]:
+    for line in input_reader.read_stripped_lines():
+        if line and line[0] != "{":
+            yield _parse_workflow(line)
+
+
+def _parse_machine_part_range(line: str) -> MachinePartRange:
+    dict_repr = line.replace("{", "{'").replace("=", "':").replace(",", ",'")
+    attribute_values = eval(dict_repr)
+    attributes = {
+        attribute_name: Interval(v, v) for attribute_name, v in attribute_values.items()
+    }
+    return MachinePartRange(attributes)
+
+
+def parse_machine_part_ranges(input_reader: InputReader) -> Iterator[MachinePartRange]:
+    for line in input_reader.read_stripped_lines():
+        if line and line[0] == "{":
+            yield _parse_machine_part_range(line)
--- models/aoc_2023/a2023_d19/tests/test_parser.py
+++ models/aoc_2023/a2023_d19/tests/test_parser.py
@@ -1,8 +1,64 @@
 from models.common.io import InputFromString
-from ..parser import parse_workflows
+from models.common.number_theory import Interval
+from ..parser import parse_workflows, parse_machine_part_ranges
+from ..logic import MachinePartRange, MachinePartState
+
+_FILE_CONTENT = """
+px{a<2006:qkq,m>2090:A,rfg}
+lnx{m>1548:A,A}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+"""
 
 
 def test_parse_workflows():
-    file_content = ""
-    input_reader = InputFromString(file_content)
-    # TODO: Implement test
+    input_reader = InputFromString(_FILE_CONTENT)
+    workflows = list(parse_workflows(input_reader))
+    assert [w.workflow_id for w in workflows] == ["px", "lnx"]
+    test_range = MachinePartRange({"a": Interval(1, 5000), "m": Interval(1, 5000)})
+    next_states = list(workflows[0].next_part_states(test_range))
+    assert next_states == [
+        MachinePartState(
+            "qkq", MachinePartRange({"a": Interval(1, 2005), "m": Interval(1, 5000)})
+        ),
+        MachinePartState(
+            "A",
+            MachinePartRange({"a": Interval(2006, 5000), "m": Interval(2091, 5000)}),
+        ),
+        MachinePartState(
+            "rfg", MachinePartRange({"a": Interval(2006, 5000), "m": Interval(1, 2090)})
+        ),
+    ]
+
+
+def test_parse_machine_part_range():
+    input_reader = InputFromString(_FILE_CONTENT)
+    ranges = list(parse_machine_part_ranges(input_reader))
+    assert ranges == [
+        MachinePartRange(
+            {
+                "x": Interval(787, 787),
+                "m": Interval(2655, 2655),
+                "a": Interval(1222, 1222),
+                "s": Interval(2876, 2876),
+            }
+        ),
+        MachinePartRange(
+            {
+                "x": Interval(1679, 1679),
+                "m": Interval(44, 44),
+                "a": Interval(2067, 2067),
+                "s": Interval(496, 496),
+            }
+        ),
+        MachinePartRange(
+            {
+                "x": Interval(2036, 2036),
+                "m": Interval(264, 264),
+                "a": Interval(79, 79),
+                "s": Interval(2244, 2244),
+            }
+        ),
+    ]
