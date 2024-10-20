8b941e9dd86a2ed7d09509fd3b6124a47547d485
Tomas Tamantini <tomasbft@gmail.com>
2024-10-20 13:00:19 -0300

    AOC 2023 - Day 20 / Part 1

--- models/aoc_2023/a2023_d20/solution.py
+++ models/aoc_2023/a2023_d20/solution.py
@@ -1,9 +1,23 @@
 from typing import Iterator
 from models.common.io import IOHandler, Problem, ProblemSolution
 from .parser import parse_module_network
+from .logic import PulseCounter, Pulse, PulseType
 
 
 def aoc_2023_d20(io_handler: IOHandler) -> Iterator[ProblemSolution]:
     problem_id = Problem(2023, 20, "Pulse Propagation")
     io_handler.output_writer.write_header(problem_id)
-    yield from []
+    initial_pulse = Pulse("button", "broadcaster", PulseType.LOW)
+    network = parse_module_network(io_handler.input_reader)
+
+    monitor = PulseCounter()
+    for _ in range(1000):
+        network.propagate(initial_pulse, monitor)
+
+    product = monitor.num_high_pulses * monitor.num_low_pulses
+    yield ProblemSolution(
+        problem_id,
+        f"The product of high and low pulses is {product}",
+        result=product,
+        part=1,
+    )
