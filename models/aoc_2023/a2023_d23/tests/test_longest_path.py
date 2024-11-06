9ee40fd0cbd8ef6eb69267b9c5c5cb8141e55fd6
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-11-06 20:04:41 -0300

    AOC 2023 - Day 23 - Write test to check algorithm efficiency

--- models/aoc_2023/a2023_d23/tests/test_longest_path.py
+++ models/aoc_2023/a2023_d23/tests/test_longest_path.py
@@ -1,3 +1,4 @@
+import pytest
 from math import inf
 from models.common.graphs import WeightedDirectedGraph
 from ..logic import max_length_non_repeating_path
@@ -13,6 +14,7 @@ def test_max_length_non_repeating_path_is_minus_inf_if_no_path():
     graph = WeightedDirectedGraph()
     graph.add_node("A")
     graph.add_node("B")
+    graph.add_edge("B", "A", 123)
     assert -inf == max_length_non_repeating_path(graph, "A", "B")
 
 
@@ -45,3 +47,26 @@ def test_max_length_non_repeating_path_does_not_visit_same_node_more_than_once()
     graph.add_edge("D", "B", 1)
     graph.add_edge("B", "E", 1)
     assert 2 == max_length_non_repeating_path(graph, "A", "E")
+
+
+@pytest.mark.skip("Waiting for implementation")
+def test_max_length_non_repeating_path_runs_efficiently():
+    graph = WeightedDirectedGraph()
+    size = 6
+    for row in range(size):
+        for col in range(size):
+            node = (row, col)
+            graph.add_node(node)
+            if row > 0:
+                node_top = (row - 1, col)
+                weight = 1 + size * row + col
+                graph.add_edge(node, node_top, weight)
+                graph.add_edge(node_top, node, weight)
+            if col > 0:
+                node_left = (row, col - 1)
+                weight = size * (1 + size * row + col)
+                graph.add_edge(node, node_left, weight)
+                graph.add_edge(node_left, node, weight)
+    start = (0, 0)
+    end = (size - 1, size - 1)
+    assert 3405 == max_length_non_repeating_path(graph, start, end)
