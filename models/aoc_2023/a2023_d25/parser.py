3e28b3bb5368b26e2d8f55a5052d00bc857189ca
Tomas Tamantini <tomasbft@gmail.com>
2024-11-09 09:15:10 -0300

    AOC 2023 - Day 25 - Implement parser

--- models/aoc_2023/a2023_d25/parser.py
+++ models/aoc_2023/a2023_d25/parser.py
@@ -1,5 +1,15 @@
 from models.common.io import InputReader
+from models.common.graphs import UndirectedGraph
 
 
-def parse_wiring_diagram(input_reader: InputReader) -> "InsertReturnType":
-    pass
+def _parse_edges(line: str, graph: UndirectedGraph) -> None:
+    node, neighbors = line.split(":")
+    for neighbor in neighbors.split():
+        graph.add_edge(node.strip(), neighbor.strip())
+
+
+def parse_wiring_diagram(input_reader: InputReader) -> UndirectedGraph:
+    graph = UndirectedGraph()
+    for line in input_reader.read_stripped_lines():
+        _parse_edges(line, graph)
+    return graph
--- models/aoc_2023/a2023_d25/tests/test_parser.py
+++ models/aoc_2023/a2023_d25/tests/test_parser.py
@@ -3,6 +3,9 @@ from ..parser import parse_wiring_diagram
 
 
 def test_parse_wiring_diagram():
-    file_content = ""
+    file_content = """aa: bb cc
+                      bb: dd"""
     input_reader = InputFromString(file_content)
-    # TODO: Implement test
+    graph = parse_wiring_diagram(input_reader)
+    assert set(graph.nodes()) == {"aa", "bb", "cc", "dd"}
+    assert set(graph.neighbors("bb")) == {"aa", "dd"}
