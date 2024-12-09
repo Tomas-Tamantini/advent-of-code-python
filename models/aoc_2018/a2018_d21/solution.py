d1e4e86146fea8d7793de28a1f6b3f6e707e962d
Tomas Tamantini <tomasbft@gmail.com>
2024-12-09 10:42:18 -0300

    AOC 2018 - Day 21 - Refactor to reduce complexity

--- models/aoc_2018/a2018_d21/solution.py
+++ models/aoc_2018/a2018_d21/solution.py
@@ -3,7 +3,27 @@ from typing import Iterator
 from models.common.io import IOHandler, Problem, ProblemSolution
 
 
-def optimized_chronal_conversion(input_num: int, exit_on_first_occurrence: bool) -> int:
+def _calculate_a(a: int, b: int) -> int:
+    new_a = a + (b & 0xFF)
+    new_a &= 0xFFFFFF
+    new_a *= 65899
+    new_a &= 0xFFFFFF
+    return new_a
+
+
+def optimized_simple_conversion(input_num: int) -> int:
+    a = 0
+    while True:
+        b = a | 0x10000
+        a = input_num
+        while True:
+            a = _calculate_a(a, b)
+            if b < 256:
+                return a
+            b //= 256
+
+
+def optimized_complex_conversion(input_num: int) -> int:
     visited = set()
     max_num_attempts = 1000
     a = 0
@@ -12,24 +32,17 @@ def optimized_chronal_conversion(input_num: int, exit_on_first_occurrence: bool)
         b = a | 0x10000
         a = input_num
         while True:
-            c = b & 0xFF
-            a += c
-            a &= 0xFFFFFF
-            a *= 65899
-            a &= 0xFFFFFF
-            if 256 > b:
-                if exit_on_first_occurrence:
-                    return a
+            a = _calculate_a(a, b)
+            if b < 256:
+                if a not in visited:
+                    visited.add(a)
+                    last_a = a
+                    max_num_attempts = 1000
                 else:
-                    if a not in visited:
-                        visited.add(a)
-                        last_a = a
-                        max_num_attempts = 1000
-                    else:
-                        max_num_attempts -= 1
-                        if max_num_attempts == 0:
-                            return last_a
-                    break
+                    max_num_attempts -= 1
+                    if max_num_attempts == 0:
+                        return last_a
+                break
             b //= 256
 
 
@@ -38,9 +51,7 @@ def aoc_2018_d21(io_handler: IOHandler) -> Iterator[ProblemSolution]:
     io_handler.output_writer.write_header(problem_id)
     lines = list(io_handler.input_reader.readlines())
     input_number = int(lines[8].split()[1])
-    register_min = optimized_chronal_conversion(
-        input_number, exit_on_first_occurrence=True
-    )
+    register_min = optimized_simple_conversion(input_number)
     yield ProblemSolution(
         problem_id,
         f"Value of register 0 to halt program with min instructions: {register_min}",
@@ -48,9 +59,7 @@ def aoc_2018_d21(io_handler: IOHandler) -> Iterator[ProblemSolution]:
         result=register_min,
     )
 
-    register_max = optimized_chronal_conversion(
-        input_number, exit_on_first_occurrence=False
-    )
+    register_max = optimized_complex_conversion(input_number)
     yield ProblemSolution(
         problem_id,
         f"Value of register 0 to halt program with max instructions: {register_max}",
