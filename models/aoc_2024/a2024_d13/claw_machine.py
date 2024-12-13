e3bb591899d948b12744a76c40dd82bb1b12e731
Tomas Tamantini <tomasbft@gmail.com>
2024-12-13 08:28:36 -0300

    AOC 2024 - Day 13 / Part 1

--- models/aoc_2024/a2024_d13/claw_machine.py
+++ models/aoc_2024/a2024_d13/claw_machine.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from models.common.vectors import Vector2D
+from models.common.vectors import Vector2D, solve_linear_system_exactly
 
 
 @dataclass(frozen=True)
@@ -8,3 +8,17 @@ class ClawMachine:
     btn_a_offset: Vector2D
     btn_b_offset: Vector2D
     prize_location: Vector2D
+
+    def num_button_presses_to_get_prize(self) -> tuple[int, int]:
+        matrix = [
+            [self.btn_a_offset.x, self.btn_b_offset.x],
+            [self.btn_a_offset.y, self.btn_b_offset.y],
+        ]
+        prize = [self.prize_location.x, self.prize_location.y]
+        solution = solve_linear_system_exactly(matrix, prize)
+        if any(sol.denominator != 1 for sol in solution) or any(
+            sol.numerator > 100 for sol in solution
+        ):
+            return None
+        else:
+            return tuple(sol.numerator for sol in solution)
--- models/aoc_2024/a2024_d13/solution.py
+++ models/aoc_2024/a2024_d13/solution.py
@@ -1,10 +1,25 @@
 from typing import Iterator
 
 from models.common.io import IOHandler, Problem, ProblemSolution
+
 from .parser import parse_claw_machines
 
 
 def aoc_2024_d13(io_handler: IOHandler) -> Iterator[ProblemSolution]:
     problem_id = Problem(2024, 13, "Claw Contraption")
     io_handler.output_writer.write_header(problem_id)
-    yield from []
+    machines = list(parse_claw_machines(io_handler.input_reader))
+    cost_btn_a = 3
+    cost_btn_b = 1
+
+    total_cost = 0
+    for machine in machines:
+        if presses := machine.num_button_presses_to_get_prize():
+            total_cost += presses[0] * cost_btn_a + presses[1] * cost_btn_b
+
+    yield ProblemSolution(
+        problem_id,
+        f"The total cost of getting all prizes is {total_cost}",
+        result=total_cost,
+        part=1,
+    )
