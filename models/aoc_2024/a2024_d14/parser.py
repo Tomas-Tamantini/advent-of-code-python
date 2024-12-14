64bf4c1449d62845a78c0fdc6f55c7dcc5332eb2
Tomas Tamantini <tomasbft@gmail.com>
2024-12-14 11:00:30 -0300

    AOC 2024 - Day 14 - Implement parser

--- models/aoc_2024/a2024_d14/parser.py
+++ models/aoc_2024/a2024_d14/parser.py
@@ -1,5 +1,18 @@
+from typing import Iterator
+
 from models.common.io import InputReader
+from models.common.vectors import Particle2D, Vector2D
 
 
-def parse_security_robots(input_reader: InputReader) -> "InsertReturnType":
-    pass
+def _parse_security_robot(line: str) -> Particle2D:
+    """p=6,3 v=-1,-3"""
+    parts = line.replace("p", "").replace("v", "").replace("=", "").split()
+    return Particle2D(
+        position=Vector2D(*map(int, parts[0].split(","))),
+        velocity=Vector2D(*map(int, parts[1].split(","))),
+    )
+
+
+def parse_security_robots(input_reader: InputReader) -> Iterator[Particle2D]:
+    for line in input_reader.read_stripped_lines():
+        yield _parse_security_robot(line)
--- models/aoc_2024/a2024_d14/tests/test_parser.py
+++ models/aoc_2024/a2024_d14/tests/test_parser.py
@@ -1,8 +1,15 @@
 from models.common.io import InputFromString
+from models.common.vectors import Particle2D, Vector2D
+
 from ..parser import parse_security_robots
 
 
 def test_parse_security_robots():
-    file_content = ""
+    file_content = """p=0,4 v=3,-3
+                      p=6,3 v=-1,-3"""
     input_reader = InputFromString(file_content)
-    # TODO: Implement test
+    robots = list(parse_security_robots(input_reader))
+    assert robots == [
+        Particle2D(position=Vector2D(0, 4), velocity=Vector2D(3, -3)),
+        Particle2D(position=Vector2D(6, 3), velocity=Vector2D(-1, -3)),
+    ]
