8d75dc560404cd85da2e9abe747f5e5531bc6cd2
Tomas Tamantini <tomasbft@gmail.com>
2024-12-15 08:55:36 -0300

    AOC 2024 - Day 15 - Refactor

--- models/aoc_2024/a2024_d15/solution.py
+++ models/aoc_2024/a2024_d15/solution.py
@@ -3,6 +3,11 @@ from typing import Iterator
 from models.common.io import IOHandler, Problem, ProblemSolution
 
 from .parser import parse_warehouse, parse_warehouse_robot_moves
+from models.common.vectors import Vector2D
+
+
+def _box_score(box: Vector2D) -> int:
+    return box.x + 100 * box.y
 
 
 def aoc_2024_d15(io_handler: IOHandler) -> Iterator[ProblemSolution]:
@@ -10,13 +15,12 @@ def aoc_2024_d15(io_handler: IOHandler) -> Iterator[ProblemSolution]:
     io_handler.output_writer.write_header(problem_id)
     warehouse = parse_warehouse(io_handler.input_reader)
 
+    next_warehouse = warehouse
     moves = list(parse_warehouse_robot_moves(io_handler.input_reader))
     for move in moves:
-        warehouse = warehouse.move_robot(move)
+        next_warehouse = next_warehouse.move_robot(move)
 
-    gps_coords = 0
-    for box in warehouse.boxes:
-        gps_coords += box.x + 100 * box.y
+    gps_coords = sum(_box_score(b) for b in next_warehouse.boxes)
 
     yield ProblemSolution(
         problem_id,
