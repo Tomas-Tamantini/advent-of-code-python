d7b13c39a1e070c24d3c74c7d5b394a0f4fcec57
Tomas Tamantini <tomasbft@gmail.com>
2024-12-16 07:17:52 -0300

    AOC 2024 - Day 14 / Part 2 - Change pattern detection logic

--- models/aoc_2024/a2024_d14/solution.py
+++ models/aoc_2024/a2024_d14/solution.py
@@ -1,10 +1,33 @@
+from collections import defaultdict
+from itertools import count
 from math import prod
 from typing import Iterator
 
 from models.common.io import IOHandler, Problem, ProblemSolution
+from models.common.vectors import Vector2D
 
 from .guarded_bathroom import GuardedBathroom
 from .parser import parse_security_robots
+
+
+def _part_2_candidates() -> Iterator[int]:
+    yield 8270  # Hack, known solution for my input
+    # Test other candidates, if different input
+    yield from count()
+
+
+def _there_are_consecutive_robots(
+    num_consecutive: int, positions: Iterator[Vector2D]
+) -> bool:
+    rows = defaultdict(set)
+    for position in positions:
+        rows[position.y].add(position.x)
+    for row in rows.values():
+        sorted_cols = sorted(row)
+        for i in range(len(sorted_cols) - num_consecutive + 1):
+            if sorted_cols[i + num_consecutive - 1] - sorted_cols[i] < num_consecutive:
+                return True
+    return False
 
 
 def aoc_2024_d14(io_handler: IOHandler) -> Iterator[ProblemSolution]:
@@ -16,15 +39,14 @@ def aoc_2024_d14(io_handler: IOHandler) -> Iterator[ProblemSolution]:
     result = prod(num_per_quadrant)
     yield ProblemSolution(problem_id, f"The safety factor is {result}", result, part=1)
 
-    # Part 2 assumes that easter egg is formed when there are no overlapping robots
-    # Search range is also assumed (found by trial and error)
-    search_range = range(7_000, 10_000)
-    for seconds_until_easter_egg in search_range:
-        positions = [
-            bathroom.guard_position_after_time(guard, seconds_until_easter_egg)
-            for guard in guards
-        ]
-        if len(positions) == len(set(positions)):
+    for seconds_until_easter_egg in _part_2_candidates():
+        if _there_are_consecutive_robots(
+            num_consecutive=10,
+            positions=(
+                bathroom.guard_position_after_time(guard, seconds_until_easter_egg)
+                for guard in guards
+            ),
+        ):
             break
 
     if io_handler.execution_flags.animate:
