21f718f2e9f600f1bbaebcbb08de5867af99c805
Tomas Tamantini <tomasbft@gmail.com>
2024-12-17 07:42:50 -0300

    AOC 2024 - Day 17 - Implement parser

--- models/aoc_2024/a2024_d17/parser.py
+++ models/aoc_2024/a2024_d17/parser.py
@@ -1,5 +1,16 @@
 from models.common.io import InputReader
 
 
-def parse_3_bit_program(input_reader: InputReader) -> "InsertReturnType":
-    pass
+def parse_3_bit_program(input_reader: InputReader) -> tuple[int, ...]:
+    for line in input_reader.read_stripped_lines():
+        if "Program" in line:
+            return tuple(map(int, line.split(":")[1].split(",")))
+
+
+def parse_3_bit_registers(input_reader: InputReader) -> dict[chr, int]:
+    registers = dict()
+    for line in input_reader.read_stripped_lines():
+        if "Register" in line:
+            register, value = line.split(":")
+            registers[register.split()[1]] = int(value)
+    return registers
--- models/aoc_2024/a2024_d17/tests/test_parser.py
+++ models/aoc_2024/a2024_d17/tests/test_parser.py
@@ -1,8 +1,22 @@
 from models.common.io import InputFromString
-from ..parser import parse_3_bit_program
+
+from ..parser import parse_3_bit_program, parse_3_bit_registers
+
+_FILE_CONTENT = """
+Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0"""
 
 
 def test_parse_3_bit_program():
-    file_content = ""
-    input_reader = InputFromString(file_content)
-    # TODO: Implement test
+    input_reader = InputFromString(_FILE_CONTENT)
+    program = parse_3_bit_program(input_reader)
+    assert (0, 1, 5, 4, 3, 0) == program
+
+
+def test_parse_3_bit_registers():
+    input_reader = InputFromString(_FILE_CONTENT)
+    registers = parse_3_bit_registers(input_reader)
+    assert {"A": 729, "B": 0, "C": 0} == registers
