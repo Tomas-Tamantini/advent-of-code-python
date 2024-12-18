16a74969e96e322f2a062137d2cb5d9d162f6a88
Tomas Tamantini <tomasbft@gmail.com>
2024-12-18 06:13:32 -0300

    AOC 2024 - Day 18 - Implement parser

--- models/aoc_2024/a2024_d18/parser.py
+++ models/aoc_2024/a2024_d18/parser.py
@@ -1,5 +1,10 @@
+from typing import Iterator
+
 from models.common.io import InputReader
+from models.common.vectors import Vector2D
 
 
-def parse_byte_positions(input_reader: InputReader) -> "InsertReturnType":
-    pass
+def parse_byte_positions(input_reader: InputReader) -> Iterator[Vector2D]:
+    for line in input_reader.read_stripped_lines():
+        x, y = map(int, line.split(","))
+        yield Vector2D(x, y)
--- models/aoc_2024/a2024_d18/tests/test_parser.py
+++ models/aoc_2024/a2024_d18/tests/test_parser.py
@@ -1,8 +1,13 @@
 from models.common.io import InputFromString
+from models.common.vectors import Vector2D
+
 from ..parser import parse_byte_positions
 
 
 def test_parse_byte_positions():
-    file_content = ""
+    file_content = """5,4
+                      4,2
+                      4,5"""
     input_reader = InputFromString(file_content)
-    # TODO: Implement test
+    positions = list(parse_byte_positions(input_reader))
+    assert positions == [Vector2D(5, 4), Vector2D(4, 2), Vector2D(4, 5)]
