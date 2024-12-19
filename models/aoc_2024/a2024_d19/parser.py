fcc04a17f36cd0d921f05e1df4bc8af3aa5d802c
Tomas Tamantini <tomasbft@gmail.com>
2024-12-19 09:42:01 -0300

    AOC 2024 - Day 19 - Implement parser

--- models/aoc_2024/a2024_d19/parser.py
+++ models/aoc_2024/a2024_d19/parser.py
@@ -1,5 +1,15 @@
+from typing import Iterator
+
 from models.common.io import InputReader
 
 
-def parse_available_patterns(input_reader: InputReader) -> "InsertReturnType":
-    pass
+def parse_available_patterns(input_reader: InputReader) -> Iterator[str]:
+    for line in input_reader.read_stripped_lines():
+        if "," in line:
+            yield from line.split(", ")
+
+
+def parse_desired_designs(input_reader: InputReader) -> Iterator[str]:
+    for line in input_reader.read_stripped_lines():
+        if "," not in line:
+            yield line
--- models/aoc_2024/a2024_d19/tests/test_parser.py
+++ models/aoc_2024/a2024_d19/tests/test_parser.py
@@ -1,8 +1,23 @@
 from models.common.io import InputFromString
-from ..parser import parse_available_patterns
+
+from ..parser import parse_available_patterns, parse_desired_designs
+
+_FILE_CONTENT = """
+                r, wr, b, g, bwu, rb, gb, br
+
+                brwrr
+                bggr
+                gbbr
+                """
 
 
 def test_parse_available_patterns():
-    file_content = ""
-    input_reader = InputFromString(file_content)
-    # TODO: Implement test
+    input_reader = InputFromString(_FILE_CONTENT)
+    available_patterns = list(parse_available_patterns(input_reader))
+    assert available_patterns == ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]
+
+
+def test_parse_desired_designs():
+    input_reader = InputFromString(_FILE_CONTENT)
+    desired_designs = list(parse_desired_designs(input_reader))
+    assert desired_designs == ["brwrr", "bggr", "gbbr"]
