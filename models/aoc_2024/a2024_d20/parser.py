3249d80b3ab412d10c7d6be360f0e64c58197485
Tomas Tamantini <tomasbft@gmail.com>
2024-12-20 06:41:05 -0300

    AOC 2024 - Day 20 - Fix parser

--- models/aoc_2024/a2024_d20/parser.py
+++ models/aoc_2024/a2024_d20/parser.py
@@ -5,9 +5,8 @@ from .logic import CpuRacetrack
 
 def parse_racetrack(input_reader: InputReader) -> CpuRacetrack:
     grid = CharacterGrid(input_reader.read())
-    return CpuRacetrack(
-        start=next(grid.positions_with_value("S")),
-        end=next(grid.positions_with_value("E")),
-        track_positions=set(grid.positions_with_value(".")),
-        wall_positions=set(grid.positions_with_value("#")),
-    )
+    start = next(grid.positions_with_value("S"))
+    end = next(grid.positions_with_value("E"))
+    wall_positions = set(grid.positions_with_value("#"))
+    track_positions = set(grid.positions_with_value(".")) | {start, end}
+    return CpuRacetrack(start, end, track_positions, wall_positions)
--- models/aoc_2024/a2024_d20/tests/test_parser.py
+++ models/aoc_2024/a2024_d20/tests/test_parser.py
@@ -14,4 +14,9 @@ def test_parse_racetrack():
     assert track._start == Vector2D(0, 0)
     assert track._end == Vector2D(2, 1)
     assert track._wall_positions == {Vector2D(2, 0), Vector2D(0, 1)}
-    assert track._track_positions == {Vector2D(1, 0), Vector2D(1, 1)}
+    assert track._track_positions == {
+        Vector2D(0, 0),
+        Vector2D(1, 0),
+        Vector2D(1, 1),
+        Vector2D(2, 1),
+    }
