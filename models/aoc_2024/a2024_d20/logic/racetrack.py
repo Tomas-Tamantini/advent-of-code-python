cf1b4f29506905e1eac5a0dd6e4491267849c497
Tomas Tamantini <tomasbft@gmail.com>
2024-12-20 09:45:18 -0300

    AOC 2024 - Day 20 / Part 1

--- models/aoc_2024/a2024_d20/logic/racetrack.py
+++ models/aoc_2024/a2024_d20/logic/racetrack.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 from typing import Iterator
 
-from models.common.graphs import explore_with_bfs, GridMaze
-from models.common.vectors import Vector2D
+from models.common.graphs import min_path_length_with_bfs
+from models.common.vectors import Vector2D, CardinalDirection
 
 
 @dataclass(frozen=True)
@@ -10,13 +10,6 @@ class _Cheat:
     start_pos: Vector2D
     end_pos: Vector2D
     saved_time: int
-
-
-@dataclass(frozen=True)
-class _RaceState:
-    position: Vector2D
-    cheat_start_pos: Vector2D | None
-    cheat_end_pos: Vector2D | None
 
 
 class CpuRacetrack:
@@ -31,39 +24,37 @@ class CpuRacetrack:
         self._end = end
         self._track_positions = track_positions
         self._wall_positions = wall_positions
+        self._cheat_position = None
 
-    def neighbors(self, node: _RaceState) -> Iterator[_RaceState]:
-        if node.position != self._end:
-            for neighbor_pos in node.position.adjacent_positions():
-                if neighbor_pos in self._track_positions:
-                    cheat_end = None if node.cheat_start_pos is None else neighbor_pos
-                    yield _RaceState(neighbor_pos, node.cheat_start_pos, cheat_end)
-                elif (
-                    neighbor_pos in self._wall_positions
-                ) and node.cheat_start_pos is None:
-                    yield _RaceState(neighbor_pos, neighbor_pos, None)
+    def neighbors(self, node: Vector2D) -> Iterator[Vector2D]:
+        if node != self._end:
+            for neighbor in node.adjacent_positions():
+                if neighbor in self._track_positions or (
+                    self._cheat_position and neighbor == self._cheat_position[0]
+                ):
+                    yield neighbor
 
-    def _min_time_without_cheat(self) -> int:
-        _maze = GridMaze()
-        for node in self._track_positions:
-            _maze.add_node_and_connect_to_neighbors(node)
-        _maze.reduce(irreducible_nodes={self._start, self._end})
-        return _maze.shortest_distance(self._start, self._end)
+    def _min_time(self) -> int:
+        return min_path_length_with_bfs(
+            self, self._start, is_final_state=lambda pos: pos == self._end
+        )
+
+    def _cheat_positions(self) -> Iterator[tuple[Vector2D, Vector2D]]:
+        for pos in self._track_positions:
+            for direction in {CardinalDirection.EAST, CardinalDirection.SOUTH}:
+                start_pos = pos.move(direction)
+                if start_pos in self._wall_positions:
+                    end_pos = start_pos.move(direction)
+                    if end_pos in self._track_positions:
+                        yield start_pos, end_pos
 
     def advantageous_cheats(self, min_saved_time: int) -> Iterator[_Cheat]:
-        time_without_cheat = self._min_time_without_cheat()
+        self._cheat_position = None
+        time_without_cheat = self._min_time()
         max_time_with_cheat = time_without_cheat - min_saved_time
-        times_with_cheat = dict()
-        initial_node = _RaceState(
-            position=self._start, cheat_start_pos=None, cheat_end_pos=None
-        )
-        for node, time in explore_with_bfs(self, initial_node):
-            if time > max_time_with_cheat:
-                break
-            if node.position == self._end:
-                times_with_cheat[(node.cheat_start_pos, node.cheat_end_pos)] = time
-
-        for cheat_pos, cheat_time in times_with_cheat.items():
-            saved_time = time_without_cheat - cheat_time
-            if saved_time > 0:
-                yield _Cheat(*cheat_pos, saved_time)
+        cheat_positions = list(self._cheat_positions())
+        for i, cheat_pos in enumerate(cheat_positions):
+            self._cheat_position = cheat_pos
+            cheat_time = self._min_time()
+            if cheat_time <= max_time_with_cheat:
+                yield _Cheat(*cheat_pos, saved_time=time_without_cheat - cheat_time)
