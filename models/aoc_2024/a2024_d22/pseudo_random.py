def2ec79270bd38132ee9eec63896c7768416382
Tomas Tamantini <tomasbft@gmail.com>
2024-12-22 10:39:15 -0300

    AOC 2024 - Day 22 - Add progress bar

--- models/aoc_2024/a2024_d22/pseudo_random.py
+++ models/aoc_2024/a2024_d22/pseudo_random.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 from typing import Iterable, Iterator
 
+from models.common.io import ProgressBar
+
 
 def _next_pseudo_random(current: int) -> int:
     mod = 16777216
@@ -20,16 +22,26 @@ def pseudo_random_sequence(seed: int, num_iterations: int) -> Iterator[int]:
         yield current
 
 
-def maximize_winnings(seeds: Iterable[int], num_iterations: int) -> int:
+def _prices_per_window(seed: int, num_iterations: int) -> dict[tuple[int, ...], int]:
     window_size = 4
+    prices = dict()
+    sequence = tuple(n % 10 for n in pseudo_random_sequence(seed, num_iterations))
+    differences = tuple((b - a) for a, b in zip(sequence, sequence[1:]))
+    for i in range(num_iterations + 1 - window_size):
+        window = differences[i : i + window_size]
+        if window not in prices:
+            prices[window] = sequence[i + window_size]
+
+    return prices
+
+
+def maximize_winnings(
+    seeds: Iterable[int], num_iterations: int, progress_bar: ProgressBar | None = None
+) -> int:
     banana_count = defaultdict(int)
-    for seed in seeds:
-        sequence = tuple(n % 10 for n in pseudo_random_sequence(seed, num_iterations))
-        differences = tuple((b - a) for a, b in zip(sequence, sequence[1:]))
-        seen = set()
-        for i in range(num_iterations + 1 - window_size):
-            window = differences[i : i + window_size]
-            if window not in seen:
-                seen.add(window)
-                banana_count[window] += sequence[i + window_size]
+    for i, seed in enumerate(seeds):
+        if progress_bar:
+            progress_bar.update(i, len(seeds))
+        for window, price in _prices_per_window(seed, num_iterations).items():
+            banana_count[window] += price
     return max(banana_count.values())
--- models/aoc_2024/a2024_d22/solution.py
+++ models/aoc_2024/a2024_d22/solution.py
@@ -9,17 +9,21 @@ def aoc_2024_d22(io_handler: IOHandler) -> Iterator[ProblemSolution]:
     problem_id = Problem(2024, 22, "Monkey Market")
     io_handler.output_writer.write_header(problem_id)
     secret_numbers = [int(num) for num in io_handler.input_reader.read_stripped_lines()]
+    num_iterations = 2000
 
     total = 0
-    for n in secret_numbers:
-        sequence = tuple(pseudo_random_sequence(n, 2000))
+    for i, n in enumerate(secret_numbers):
+        io_handler.progress_bar.update(i, len(secret_numbers))
+        sequence = tuple(pseudo_random_sequence(n, num_iterations))
         total += sequence[-1]
 
     yield ProblemSolution(
         problem_id, f"The sum of generated numbers is {total}", result=total, part=1
     )
 
-    max_winnings = maximize_winnings(secret_numbers, 2000)
+    max_winnings = maximize_winnings(
+        secret_numbers, num_iterations, io_handler.progress_bar
+    )
     yield ProblemSolution(
         problem_id,
         f"The maximum winnings are {max_winnings}",
